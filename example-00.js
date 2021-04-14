79d1b4cffd5065ba4b902fd21d5e2c199a109414
Jonathan Pool <jonathan.pool@cvshealth.com>
2021-04-14 14:01:50 -0400

    Developed initial web UI

---
+++ example-00.js
@@ -0,0 +1,38 @@
+// Handles a form submission.
+exports.formHandler = globals => {
+  // Add the manager username and computed email to the query.
+  const {query} = globals;
+  if (globals.queryIncludes(['url', 'elementType', 'elementIndex'])) {
+    const {chromium, firefox, webkit} = require('playwright');
+    (async () => {
+      [chromium, firefox, webkit].forEach(async agent => {
+        const ui = await agent.launch();
+        const page = await ui.newPage();
+        await page.goto(query.url);
+        const element = await page.$(':nth-match(query.elementType, elementIndex)');
+        const elementBox = await element.boundingBox();
+        const shotBox = {
+          x: elementBox.x - 10,
+          y: elementBox.y - 10,
+          width: elementBox.width + 20,
+          height: elementBox.height + 20
+        };
+        await page.screenshot({
+          clip: shotBox,
+          path: `link-blur-${agent.name()}.png`
+        });
+        await element.focus();
+        await page.screenshot({
+          clip: shotBox,
+          path: `screenShots/example-00-focus-${agent.name()}.png`
+        });
+        await ui.close();
+      });
+    })();
+    // Replace the placeholders and serve the step-2 view.
+    globals.render('example-00-out', true);
+  }
+  else {
+    globals.serveMessage('ERROR: Some information missing or invalid.', globals.response);
+  }
+};
---
+++ index.js
@@ -0,0 +1,246 @@
+/*
+  index.js
+  autotest main script.
+*/
+const globals = {};
+// ########## IMPORTS
+// Module to access files.
+globals.fs = require('fs').promises;
+// Module to keep secrets local.
+require('dotenv').config();
+// Module to create an HTTP server and client.
+const http = require('http');
+// Module to create an HTTPS server and client.
+const https = require('https');
+// ########## CONSTANTS
+globals.urlStart = `${process.env.PROTOCOL}://${process.env.HOST}`;
+const protocol = process.env.PROTOCOL || 'https';
+const mimeTypes = {
+  '/example-00.html': 'text/html',
+  '/index.html': 'text/html',
+  '/style.css': 'text/css'
+};
+const redirects = {
+  '/': '/autotest/index.html'
+};
+const customErrorPageStart = [
+  '<html lang="en-US">',
+  '  <head>',
+  '    <meta charset="utf-8">',
+  '    <title>ERROR</title>',
+  '  </head>',
+  '  <body><main>',
+  '    <h1>ERROR</h1>',
+  '    <p>__msg__</p>'
+];
+const systemErrorPageStart = customErrorPageStart.concat(...[
+  '    <p>Location:</p>',
+  '    <pre>__stack__</pre>'
+]);
+const errorPageEnd = [
+  '  </main></body>',
+  '</html>',
+  ''
+];
+const customErrorPage = customErrorPageStart.concat(...errorPageEnd);
+const systemErrorPage = systemErrorPageStart.concat(...errorPageEnd);
+// ########## FUNCTIONS
+// Serves a system error message.
+globals.serveError = (error, response) => {
+  if (response.writableEnded) {
+    console.log(error.message);
+    console.log(error.stack);
+  }
+  else {
+    response.statusCode = 400;
+    response.write(
+      systemErrorPage
+      .join('\n')
+      .replace('__msg__', error.message)
+      .replace('__stack__', error.stack)
+    );
+    response.end();
+  }
+  return '';
+};
+// Serves a custom error message.
+globals.serveMessage = (msg, response) => {
+  if (response.writableEnded) {
+    console.log(msg);
+  }
+  else {
+    response.statusCode = 400;
+    response.write(customErrorPage.join('\n').replace('__msg__', msg));
+    response.end();
+  }
+  return '';
+};
+// Replaces a select-list placeholder.
+globals.fillSelect = (string, placeholder, sourceObject, selected) => {
+  return string.replace(
+    new RegExp(`( *)${placeholder}`),
+    Object
+    .keys(sourceObject)
+    .map(
+      key => {
+        const selAttr = key === selected ? 'selected ' : '';
+        const replacement = sourceObject[key].name || sourceObject[key];
+        return `$1<option ${selAttr}value="${key}">${replacement}</option>`;
+      }
+    )
+    .join('\n')
+  );
+};
+// Serves a page.
+globals.servePage = (content, newURL, mimeType, response) => {
+  response.setHeader('Content-Type', mimeType);
+  if (newURL) {
+    response.setHeader('Content-Location', newURL);
+  }
+  response.end(content);
+};
+// Returns whether each specified query parameter is truthy.
+globals.queryIncludes = params => params.every(param => globals.query[param]);
+// Replaces the placeholders in a page and optionally serves the page.
+globals.render = (pageName, isServable) => {
+  // Get the page.
+  return globals.fs.readFile(`${pageName}.html`, 'utf8')
+  .then(
+    // When it arrives:
+    page => {
+      // Replace its placeholders with eponymous query parameters.
+      const renderedPage = page.replace(/__([a-zA-Z]+)__/g, (ph, qp) => globals.query[qp]);
+      // If the page is ready to serve:
+      if (isServable) {
+        // Serve it.
+        globals.servePage(renderedPage, `/${pageName}.html`, 'text/html', globals.response);
+        return '';
+      }
+      // Otherwise, i.e. if the page needs modification before it is served:
+      else {
+        // Return the rendered page.
+        return renderedPage;
+      }
+    },
+    error => globals.serveError(new Error(error), globals.response)
+  );
+};
+// Serves a redirection.
+globals.redirect = (url, response) => {
+  response.statusCode = 303;
+  response.setHeader('Location', url);
+  response.end();
+};
+// Handles a request.
+const requestHandler = (request, response) => {
+  const {method} = request;
+  const bodyParts = [];
+  request.on('error', err => {
+    console.error(err);
+  })
+  .on('data', chunk => {
+    bodyParts.push(chunk);
+  })
+  // When the request has arrived:
+  .on('end', () => {
+    // Identify its WHATWG URL instance.
+    let url = new URL(request.url, `${protocol}://${request.headers.host}`);
+    // Identify the pathname, with any initial 'autotest/' segment deleted.
+    let pathName = url.pathname;
+    if (pathName.startsWith('/autotest/')) {
+      pathName = pathName.slice(9);
+    }
+    else if (pathName === '/autotest') {
+      pathName = '/';
+    }
+    let searchParams = {};
+    globals.query = {};
+    globals.response = response;
+    // If the request method is GET:
+    if (method === 'GET') {
+      // Identify a query object, presupposing no query name occurs twice.
+      searchParams = url.searchParams;
+      searchParams.forEach((value, name) => globals.query[name] = value);
+      const type = mimeTypes[pathName];
+      const target = redirects[pathName];
+      // If a requestable static file is requested:
+      if (type) {
+        // Get the file content.
+        globals.fs.readFile(pathName.slice(1), 'utf8')
+        .then(
+          // When it has arrived, serve it.
+          content => globals.servePage(content, pathName, type, response),
+          error => globals.serveError(new Error(error), response)
+        );
+      }
+      // Otherwise, if the request must be redirected:
+      else if (target) {
+        // Redirect it.
+        globals.redirect(target, response);
+      }
+      // Otherwise, if the site icon was requested:
+      else if (pathName === '/favicon.ico') {
+        // Get the file content.
+        globals.fs.readFile('favicon.ico')
+        .then(
+          // When it has arrived:
+          content => {
+            // Serve it.
+            response.setHeader('Content-Type', 'image/x-icon');
+            response.write(content, 'binary');
+            response.end();
+          },
+          error => globals.serveError(new Error(error), response)
+        );
+      }
+      // Otherwise, i.e. if the URL is invalid:
+      else {
+        globals.serveMessage('ERROR: Invalid URL.', response);
+      }
+    }
+    // Otherwise, if the request method is POST:
+    else if (method === 'POST') {
+      // Get a query string from the request body.
+      const queryString = Buffer.concat(bodyParts).toString();
+      // Identify a query object.
+      searchParams = new URLSearchParams(queryString);
+      searchParams.forEach((value, name) => globals.query[name] = value);
+      // If the request submitted the first step of an example:
+      if (/^\/example-\d0$/.test(pathName)) {
+        // Process the submission.
+        require(`./example-${pathName.slice(-2)}`).formHandler(globals);
+      }
+      // Otherwise, i.e. if the request was invalid:
+      else {
+        // Serve an error message.
+        globals.serveMessage('ERROR: Form submission invalid.', response);
+      }
+    }
+  });
+};
+// ########## SERVER
+const serve = (protocolModule, options) => {
+  const server = protocolModule.createServer(options, requestHandler);
+  const port = process.env.PORT || '3000';
+  server.listen(port, () => {
+    console.log(`Server listening at ${protocol}://localhost:${port}.`);
+  });
+};
+if (protocol === 'http') {
+  serve(http, {});
+}
+else if (protocol === 'https') {
+  globals.fs.readFile(process.env.KEY)
+  .then(
+    key => {
+      globals.fs.readFile(process.env.CERT)
+      .then(
+        cert => {
+          serve(https, {key, cert});
+        },
+        error => console.log(error.message)
+      );
+    },
+    error => console.log(error.message)
+  );
+}
