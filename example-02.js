3c88512c75f81f57f5eecf30b51f8e10dc844e95
Jonathan Pool <jonathan.pool@cvshealth.com>
2021-04-23 08:12:27 -0400

    Improved 02 output and commenting

--- example-02.js
+++ example-02.js
@@ -7,14 +7,19 @@ exports.formHandler = globals => {
       const ui = await chromium.launch();
       const page = await ui.newPage();
       await page.goto(query.url);
+      // Get an array of ElementHandles for elements with explicit role attributes.
       const elements = await page.$$('[role]');
       const tally = {};
       let done = 0;
+      // For each of the elements, in parallel and in random order:
       elements.forEach(async element => {
+        // Get its type.
         const tagHandle = await element.getProperty('tagName');
         const ucTag = await tagHandle.jsonValue();
         const tag = ucTag.toLowerCase();
+        // Get its explicit role.
         const role = await element.getAttribute('role');
+        // Add the element to the tabulation.
         if (tally[tag]) {
           if (tally[tag][role]) {
             tally[tag][role]++;
@@ -28,7 +33,9 @@ exports.formHandler = globals => {
             [role]: 1
           };
         }
+        // If the element is the last one processed:
         if (++done === elements.length) {
+          // Output the report.
           query.tally = JSON.stringify(tally, null, 2);
           globals.render('example-02-out', true);
         }
