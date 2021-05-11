532b367a9f1938a10d3a3877797deb4c94b97c52
Jonathan Pool <jonathan.pool@cvshealth.com>
2021-05-11 17:50:48 -0400

    Refactored role

--- tests/role/app.js
+++ tests/role/app.js
@@ -1,59 +1,19 @@
-// Handles a form submission.
-exports.formHandler = globals => {
-  const {query} = globals;
-  if (globals.queryIncludes(['actFileOrURL'])) {
-    const debug = false;
-    (async () => {
-      // Perform the specified preparations.
-      const page = await globals.perform(debug);
-      // Compile an axe-core report.
-      await globals.axe(page, ['aria-roles', 'aria-allowed-role']);
-      // Identify an array of the ElementHandles of elements with role attributes.
-      const elements = await page.$$('[role]');
-      const list = [];
-      // If any exist:
-      if (elements.length) {
-        // For each ElementHandle:
-        elements.forEach(async (element, index) => {
-          // Identify a JSHandle for its type.
-          const tagHandle = await element.getProperty('tagName');
-          // Identify the upper-case name of the type.
-          const ucTag = await tagHandle.jsonValue();
-          // Identify its conversion to lower case.
-          const tag = ucTag.toLowerCase();
-          // Identify the value of its role attribute.
-          const role = await element.getAttribute('role');
-          // Add the index, type, and role to the list.
-          list.push([index, tag, role]);
-          // If all matching elements have been processed:
-          if (list.length === elements.length) {
-            // Sort the list by index.
-            list.sort((a, b) => a[0] - b[0]);
-            // Convert it to an array of HTML list elements.
-            const htmlList = [];
-            list.forEach(item => {
-              htmlList.push(
-                `<li>
-                  ${item[0]}. Element <code>${item[1]}</code> has role <code>${item[2]}</code>.
-                </li>`
-              );
-            });
-            // Concatenate the array elements.
-            query.report = htmlList.join('\n            ');
-            // Render and serve a report.
-            globals.render('role', true);
-          }
-        });
-      }
-      // Otherwise, i.e. if no elements with role attributes exist:
-      else {
-        // Render and serve a report.
-        query.report = '<li><strong>None</strong></li>';
-        globals.render('role', true);
-      }
-    })();
-  }
-  else {
-    globals.serveMessage('ERROR: Some information missing or invalid.', globals.response);
-  }
+// Compiles a report.
+exports.reporter = async page => {
+  // Get an array of data on all elements with role attributes.
+  const data = await page.$eval('body', body => {
+    const elements = Array.from(body.querySelectorAll('[role]:not([role=""])'));
+    return elements.map((element, index) => {
+      const liParts = {
+        index: `${index}.`,
+        type: `Element: <code>${element.tagName.toLowerCase()}</code>.`,
+        role: `Role: <code>${element.getAttribute('role')}</code>.`
+      };
+      return `<li>${liParts.join(' ')}</li>`;
+    });
+  });
+  return {
+    json: false,
+    data
+  };
 };
