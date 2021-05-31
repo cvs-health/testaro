b4b03b90a6772c7cab25a524db43c3029b6c5f8e
Jonathan Pool <pool@stulta.com>
2021-05-31 14:44:57 -0400

    Added orgurls feature

---
+++ orgurls.js
@@ -0,0 +1,90 @@
+/*
+  orgurls.js
+  Adds URLs to organization names via the KickFire API.
+*/
+// ########## IMPORTS
+// Module to access files.
+const fs = require('fs').promises;
+// Module to keep secrets local.
+require('dotenv').config();
+// Module to create an HTTPS server and client.
+const https = require('https');
+// ########## CONSTANTS
+const isInUS = true;
+// ########## FUNCTIONS
+// Gets the URL of the website of an organization.
+const getURL = orgName => {
+  const kickFireKey = process.env.KICKFIRE_KEY;
+  // Get the data from KickFire.
+  return new Promise(resolve => {
+    const countryParam = isInUS ? '&countryShort=US' : '';
+    https.get(
+      {
+        host: 'api.kickfire.com',
+        path: `/v1/name2website?key=${kickFireKey}&name=${orgName}${countryParam}`,
+        protocol: 'https:'
+      },
+      response => {
+        let reportText = '';
+        response.on('data', chunk => {
+          reportText += chunk;
+        });
+        // When the data arrive, return them as an object.
+        response.on('end', () => {
+          try {
+            const report = JSON.parse(reportText);
+            if (report.status === 'success' && report.results > 0) {
+              return resolve(
+                report.data.filter((result, index) => index === 0 || result.matchRate === 100)
+              );
+            }
+            else {
+              return resolve([{
+                error: 'KickFire found no matches.',
+                reportText: 'NONE'
+              }]);
+            }
+          }
+          catch (error) {
+            return resolve([{
+              error: 'KickFire did not return JSON.',
+              reportText
+            }]);
+          }
+        });
+      }
+    );
+  });
+};
+// Adds URLs to an array of organization names.
+const addURLs = async (orgData, orgNames, index) => {
+  if (index < orgNames.length) {
+    const orgName = orgNames[index];
+    orgData[index] = await getURL(orgName);
+    return await addURLs(orgData, orgNames, ++index);
+  }
+  else {
+    return Promise.resolve('');
+  }
+};
+// Adds URLs to orgnames.txt.
+const createData = async () => {
+  const orgNameList = await fs.readFile('orgnames.txt', 'utf8');
+  if (orgNameList.length) {
+    const orgNames = orgNameList.split('\n').filter(name => name.trim().length);
+    if (orgNames.length) {
+      const orgData = [];
+      await addURLs(orgData, orgNames, 0);
+      fs.writeFile('orgurls.json', JSON.stringify(orgData, null, 2));
+      console.log('File orgurls.json created.');
+    }
+    else {
+      console.log('ERROR: No organization names found.');
+    }
+  }
+  else {
+    console.log('ERROR: Organization names file empty.');
+  }
+};
+// ########## OPERATION
+createData();
