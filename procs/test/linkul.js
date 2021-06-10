06c2233b9ebab045893be8c6666a98e46df5052e
Jonathan Pool <jonathan.pool@cvshealth.com>
2021-06-10 08:54:14 -0400

    Ignored blank links

--- procs/test/linkul.js
+++ procs/test/linkul.js
@@ -1,15 +1,5 @@
-// Reports counts and fractions of underlined links among inline links adn lists them.
+// Returns counts, fractions, and texts of inline links, by whether underlined.
 exports.linkul = async page => await page.$eval('body', body => {
-  // Recursively returns the first ancestor element with non-inline display.
-  const blockOf = node => {
-    const parentElement = node.parentElement;
-    if (window.getComputedStyle(parentElement).display.startsWith('inline')) {
-      return blockOf(parentElement);
-    }
-    else {
-      return parentElement;
-    }
-  };
   // Returns whether all child elements of an element have inline display.
   const isInline = element => {
     const children = Array.from(element.children);
@@ -17,12 +7,29 @@ exports.linkul = async page => await page.$eval('body', body => {
   };
   // Removes spacing characters from a text.
   const despace = text => text.replace(/\s/g, '');
-  // Returns whether one element has more text than another.
-  const hasMoreText = (element0, element1) =>
-    despace(element0.textContent).length > despace(element1.textContent).length;
+  // Returns whether one element has more text than another and both have real text.
+  const hasMoreText = element => {
+  // Recursively returns the first ancestor element with non-inline display.
+    const blockOf = node => {
+      const parentElement = node.parentElement;
+      if (window.getComputedStyle(parentElement).display.startsWith('inline')) {
+        return blockOf(parentElement);
+      }
+      else {
+        return parentElement;
+      }
+    };
+    const elementText = despace(element.textContent);
+    if (elementText) {
+      return despace(blockOf(element).textContent).length > elementText.length;
+    }
+    else {
+      return false;
+    }
+  };
   const compact = string => string.replace(/[\t\n]/g, '').replace(/\s{2,}/g, ' ').trim();
   const links = Array.from(body.getElementsByTagName('a'));
-  const inLinks = links.filter(link => isInline(link) && hasMoreText(blockOf(link), link));
+  const inLinks = links.filter(link => isInline(link) && hasMoreText(link));
   const ulInLinkTexts = [];
   const nulInLinkTexts = [];
   inLinks.forEach(link => {
