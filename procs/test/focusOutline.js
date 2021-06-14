a54bb081ae24c76e45b443748af2abd023b7e8b6
Jonathan Pool <pool@stulta.com>
2021-06-14 06:50:17 -0400

    Started arrow-key focusOutline

---
+++ procs/test/focusOutline.js
@@ -0,0 +1,25 @@
+// Reports on the outline and the text of the active element.
+exports.focusedData = async (page, groupFirst) => page.evaluate(groupFirst => {
+  // Initialize the result.
+  const result = {};
+  // Identify the focused element.
+  const focused = document.activeElement;
+  // If it is a focusable element in the page:
+  if (focused && focused !== document.body) {
+  }
+}, groupFirst);
+    const outlineWidth = window.getComputedStyle(focused).outlineWidth;
+    if (outlineWidth !== '0px') {
+      return [true, 1, 1];
+    }
+    else {
+      return [true, 1, 0];
+    }
+  }
+  else {
+    return [false, 0, 0];
+  }
+});
+focusInDoc = verdict[0];
+focusables += verdict[1];
+outlined += verdict[2];
