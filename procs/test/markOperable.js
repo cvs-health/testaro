eb709b99e3c2c7a7f8142e04983b6642b94ed4e6
Jonathan Pool <jonathan.pool@cvshealth.com>
2021-06-17 17:13:03 -0400

    Finished implementing focusable test

--- procs/test/markOperable.js
+++ procs/test/markOperable.js
@@ -14,7 +14,7 @@ exports.markOperable = async page => {
       if (! element.dataset.autotestOperable) {
         element.setAttribute('data-autotest-operable', 1);
       }
-    });
+    }, element);
   };
   // Recursively finds and marks the elements that have operable tag names.
   const tagOperable = async (page, elements) => {
@@ -25,95 +25,48 @@ exports.markOperable = async page => {
       // Get its tag name.
       const tagNameJSHandle = await firstElement.getProperty('tagName');
       const tagName = await tagNameJSHandle.jsonValue();
+      // If the tag name is inherently operable:
       if (opTags.includes(tagName)) {
+        // Mark the element as operable.
         await mark(page, firstElement);
       }
-      // Determine whether 
+      // Process the remaining elements.
+      await tagOperable(page, elements.slice(1));
     }
   };
 
-  // Get ElementHandles for all elements in the body.
-  const allElements = await page.$$('body *');
-
-  // Navigation-key sequence.
-  const nextNavKeys = {
-    Tab: ['ArrowRight', null],
-    ArrowRight: ['ArrowRight', 'ArrowDown'],
-    ArrowDown: ['ArrowDown', 'Tab']
-  };
-
-  // ### FUNCTIONS
-
-  // Identifies and marks the focused in-body element or identifies a failure status.
-  const focused = async () => {
-    // Identify a JSHandle of the focused element or a failure status.
-    const focusJSHandle = await page.evaluateHandle(() => {
-      // Identify the focused element.
-      const focus = document.activeElement;
-      // If it exists and is within the body:
-      if (focus && focus !== document.body) {
-        // If it has not previously been focused:
-        if (! focus.dataset.autotestFocused) {
-          // Mark it as previously focused.
-          focus.setAttribute('data-autotest-focused', '1');
-          // Return it.
-          return focus;
-        }
-        // Otherwise, i.e. if it has been previously focused:
-        else {
-          // Return a status message.
-          return {atFocusStatus: 'already'};
-        }
+  // Recursively finds and marks the elements that are clickable.
+  const clickOperable = async (page, elements) => {
+    // If any elements remain unprocessed:
+    if (elements.length) {
+      // Identify the first of them.
+      const firstElement = elements[0];
+      // If it is clickable:
+      try {
+        await firstElement.click({
+          timeout: 100,
+          trial: true
+        });
+        // Mark it as operable.
+        await mark(page, firstElement);
       }
-      // Otherwise, i.e. if it does not exist or is the body itself:
-      else {
-        // Return a status message.
-        return {atFocusStatus: 'no'};
+      // Otherwise, i.e. if it is not clickable, do nothing.
+      catch {
+        '';
       }
-    });
-    // Get the failure status.
-    const statusHandle = await focusJSHandle.getProperty('atFocusStatus');
-    const status = await statusHandle.jsonValue();
-    // If there is one:
-    if (status) {
-      // Return it.
-      return status;
-    }
-    // Otherwise, i.e. if an element within the body is newly focused:
-    else {
-      // Return its ElementHandle.
-      return focusJSHandle.asElement();
-    }
-  };
-  // Recursively focuses and marks elements.
-  const markFocused = async () => {
-    // Identify and mark the newly focused element or identify a failure.
-    const focus = await focused();
-    // Identify the failure status, if any.
-    const failure = typeof focus === 'string' ? focus : null;
-    // If it is a refocused element and the last navigation key was an arrow key:
-    if (failure === 'already' && ['ArrowRight', 'ArrowDown'].includes(lastNavKey)) {
-      // Press the next post-failure navigation key.
-      await page.keyboard.press(lastNavKey = nextNavKeys[lastNavKey][1]);
-      // Process the element focused by that keypress.
-      await markFocused();
-    }
-    // Otherwise, if there is no failure:
-    else if (! failure) {
-      // Press the next post-success navigation key.
-      await page.keyboard.press(lastNavKey = nextNavKeys[lastNavKey][0]);
-      // Process the element focused by that keypress.
-      await markFocused();
+      // Process the remaining elements.
+      await clickOperable(page, elements.slice(1));
     }
   };
 
   // ### OPERATION
 
-  // Press the Tab key and identify it as the last-pressed navigation key.
-  await page.keyboard.press('Tab');
-  let lastNavKey = 'Tab';
-  // Recursively focus and mark elements.
-  await markFocused();
+  // Identify the elements in the body.
+  const elements = await page.$$('body *');
+  // Recursively mark elements with operable tag names as operable.
+  await tagOperable(page, elements);
+  // Recursively mark clickable elements as operable.
+  await clickOperable(page, elements);
   // Return the result.
   return 1;
 };
---
+++ tests/focusable/app.js
@@ -0,0 +1,35 @@
+// Tabulates and lists focusable and operable elements.
+exports.reporter = async page => {
+  // Import a module to get the texts of an element.
+  const {allText} = require('../procs/allText');
+  // Mark the focusable elements.
+  await require('../../procs/test/markFocusable').markFocusable(page);
+  // Mark the operable elements.
+  await require('../../procs/test/markOperable').markOperable(page);
+  // Get an array of the elements that are focusable but not operable.
+  const fNotO = await page.$$('[data-autotest-focused]not:([data-autotest-operable])');
+  // Get an array of the elements that are operable but not focusable.
+  const oNotF = await page.$$('[data-autotest-operable]not:([data-autotest-focused])');
+  // Get an array of the elements that are focusable and operable.
+  const fAndO = await page.$$('[data-autotest-focused][data-autotest-operable])');
+  // Gets the tag name and texts of an element.
+  const tagAndText = async element => {
+    const tagNameJSHandle = await element.getProperty('tagName');
+    const tagName = await tagNameJSHandle.jsonValue();
+    const text = await allText(element);
+    return {
+      tagName,
+      text
+    };
+  };
+  const fNotOTexts = fNotO.map(element => tagAndText(element));
+  const oNotFTexts = oNotF.map(element => tagAndText(element));
+  const fAndOTexts = fAndO.map(element => tagAndText(element));
+  return {
+    result: {
+      operableButNotFocusable: oNotFTexts,
+      focusableButNotOperable: fNotOTexts,
+      focusableAndOperable: fAndOTexts
+    }
+  };
+};
