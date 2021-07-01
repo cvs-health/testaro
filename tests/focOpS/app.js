c1dfa52d165de94b4cb062fa1fcffda1b0229d8b
Jonathan Pool <jonathan.pool@cvshealth.com>
2021-07-01 16:39:52 -0400

    Cleaned focOpS

--- tests/focOpS/app.js
+++ tests/focOpS/app.js
@@ -1,5 +1,6 @@
 // Tabulates focusable and operable elements.
 exports.reporter = async page => {
   const report = await require('../../procs/test/focOp').focOp(page, false);
+  delete report.result.items;
   return report;
 };
