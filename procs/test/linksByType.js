730364709dcbf739e1a5c9191eca9c67d9484578
Jonathan Pool <jonathan.pool@cvshealth.com>
2021-08-04 13:40:17 -0400

    Refactored link classification

---
+++ procs/test/linksByType.js
@@ -0,0 +1,50 @@
+// Returns an array of the links in a page, classified as inline or block.
+exports.linksByType = async page => await page.evaluateHandle(() => {
+  // FUNCTION DEFINITIONS START
+  // Returns whether all child elements of an element have inline display.
+  const isInline = element => {
+    const children = Array.from(element.children);
+    return children.every(child => window.getComputedStyle(child).display.startsWith('inline'));
+  };
+  // Removes spacing characters from a text.
+  const despace = text => text.replace(/\s/g, '');
+  // Returns whether the nearest block has more text than an element and both have real text.
+  const hasAdjacentText = element => {
+    // Recursively returns the first ancestor element with non-inline display.
+    const blockOf = node => {
+      const parentElement = node.parentElement;
+      if (window.getComputedStyle(parentElement).display.startsWith('inline')) {
+        return blockOf(parentElement);
+      }
+      else {
+        return parentElement;
+      }
+    };
+    const elementText = despace(element.textContent);
+    if (elementText) {
+      return despace(blockOf(element).textContent).length > elementText.length;
+    }
+    else {
+      return false;
+    }
+  };
+  // FUNCTION DEFINITIONS END
+  // Get the links in the page.
+  const links = Array.from(document.getElementsByTagName('a'));
+  // Initialize an object classifying the links.
+  const linkTypes = {
+    inline: [],
+    block: []
+  };
+  // Populate it.
+  links.forEach(link => {
+    if (isInline(link) && hasAdjacentText(link)) {
+      linkTypes.inline.push(link);
+    }
+    else {
+      linkTypes.block.push(link);
+    }
+  });
+  // Return it.
+  return linkTypes;
+});
--- procs/test/linkul.js
+++ procs/test/linkul.js
@@ -1,74 +1,51 @@
 // Returns counts, fractions, and texts of inline links, by whether underlined.
-exports.linkUl = async (page, withItems) => await page.$eval('body', (body, withItems) => {
-  // FUNCTION DEFINITIONS START
-  // Returns whether all child elements of an element have inline display.
-  const isInline = element => {
-    const children = Array.from(element.children);
-    return children.every(child => window.getComputedStyle(child).display.startsWith('inline'));
-  };
-  // Removes spacing characters from a text.
-  const despace = text => text.replace(/\s/g, '');
-  // Returns whether one element has more text than another and both have real text.
-  const hasMoreText = element => {
-    // Recursively returns the first ancestor element with non-inline display.
-    const blockOf = node => {
-      const parentElement = node.parentElement;
-      if (window.getComputedStyle(parentElement).display.startsWith('inline')) {
-        return blockOf(parentElement);
+exports.linkUl = async (page, withItems) => {
+  // Identify the links in the page, by type.
+  const linkTypes = await require('./linksByType').linksByType(page);
+  return await page.evaluate(args => {
+    const withItems = args[0];
+    const linkTypes = args[1];
+    // FUNCTION DEFINITION START
+    // Returns a space-minimized copy of a string.
+    const compact = string => string.replace(/[\t\n]/g, '').replace(/\s{2,}/g, ' ').trim();
+    // FUNCTION DEFINITION END
+    // Identify the inline links.
+    const inLinks = linkTypes.inline;
+    const inLinkCount = inLinks.length;
+    let underlined = 0;
+    const ulInLinkTexts = [];
+    const nulInLinkTexts = [];
+    // For each of them:
+    inLinks.forEach(link => {
+      // Identify the text of the link if itemization is required.
+      const text = withItems ? compact(link.textContent) : '';
+      // If it is underlined:
+      if (window.getComputedStyle(link).textDecorationLine === 'underline') {
+        // Increment the count of underlined inline links.
+        underlined++;
+        // If required, add its text to the array of their texts.
+        if (withItems) {
+          ulInLinkTexts.push(text);
+        }
       }
-      else {
-        return parentElement;
+      // Otherwise, if it is not underlined and itemization is required:
+      else if (withItems) {
+        // Add its text to the array of texts of non-underlined inline links.
+        nulInLinkTexts.push(text);
       }
+    });
+    // Get the percentage of underlined links among all inline links.
+    const ulPercent = inLinkCount ? Math.floor(100 * underlined / inLinkCount) : 'N/A';
+    const data = {
+      linkCount: inLinks.length + linkTypes.block.length,
+      inLinkCount,
+      underlined,
+      ulPercent
     };
-    const elementText = despace(element.textContent);
-    if (elementText) {
-      return despace(blockOf(element).textContent).length > elementText.length;
+    if (withItems) {
+      data.ulInLinkTexts = ulInLinkTexts;
+      data.nulInLinkTexts = nulInLinkTexts;
     }
-    else {
-      return false;
-    }
-  };
-  // Returns a space-minimized copy of a string.
-  const compact = string => string.replace(/[\t\n]/g, '').replace(/\s{2,}/g, ' ').trim();
-  // FUNCTION DEFINITIONS END
-  // Identify all links.
-  const links = Array.from(body.getElementsByTagName('a'));
-  // Identify those with less text than their nearest non-inline ancestors as inline.
-  const inLinks = links.filter(link => isInline(link) && hasMoreText(link));
-  const inLinkCount = inLinks.length;
-  let underlined = 0;
-  const ulInLinkTexts = [];
-  const nulInLinkTexts = [];
-  // For each of them:
-  inLinks.forEach(link => {
-    // Identify the text of the link if itemization is required.
-    const text = withItems ? compact(link.textContent) : '';
-    // If it is underlined:
-    if (window.getComputedStyle(link).textDecorationLine === 'underline') {
-      // Increment the count of underlined inline links.
-      underlined++;
-      // If required, add its text to the array of their texts.
-      if (withItems) {
-        ulInLinkTexts.push(text);
-      }
-    }
-    // Otherwise, if it is not underlined and itemization is required:
-    else if (withItems) {
-      // Add its text to the array of texts of non-underlined inline links.
-      nulInLinkTexts.push(text);
-    }
-  });
-  // Get the percentage of underlined links among all inline links.
-  const ulPercent = inLinkCount ? Math.floor(100 * underlined / inLinkCount) : 'N/A';
-  const data = {
-    linkCount: links.length,
-    inLinkCount,
-    underlined,
-    ulPercent
-  };
-  if (withItems) {
-    data.ulInLinkTexts = ulInLinkTexts;
-    data.nulInLinkTexts = nulInLinkTexts;
-  }
-  return data;
-}, withItems);
+    return data;
+  }, [withItems, linkTypes]);
+};
