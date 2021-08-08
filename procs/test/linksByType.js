efe0323bdd2978afcea260514795b360c1ca85a7
Jonathan Pool <pool@stulta.com>
2021-08-08 14:00:09 -0400

    Added flex display to inline-link category

--- procs/test/linksByType.js
+++ procs/test/linksByType.js
@@ -1,19 +1,23 @@
-// Returns an object classifying the links in a page as inline or block.
+// Returns an object classifying the links in a page by layout.
 exports.linksByType = async page => await page.evaluateHandle(() => {
   // FUNCTION DEFINITIONS START
-  // Returns whether all child elements of an element have inline display.
-  const isInline = element => {
+  // Returns whether all child elements of an element have inline or flex display.
+  const isFluid = element => {
     const children = Array.from(element.children);
-    return children.every(child => window.getComputedStyle(child).display.startsWith('inline'));
+    return children.every(child => {
+      const childDisplay = window.getComputedStyle(child).display;
+      return childDisplay.startsWith('inline') || childDisplay.startsWith('flex');
+    });
   };
   // Removes spacing characters from a text.
   const despace = text => text.replace(/\s/g, '');
   // Returns whether the nearest block has more text than an element and both have real text.
   const hasAdjacentText = element => {
-    // Recursively returns the first ancestor element with non-inline display.
+    // Recursively returns the first ancestor element with non-fluid display.
     const blockOf = node => {
       const parentElement = node.parentElement;
-      if (window.getComputedStyle(parentElement).display.startsWith('inline')) {
+      const parentDisplay = window.getComputedStyle(parentElement).display;
+      if (parentDisplay.startsWith('inline') || parentDisplay.startsWith('flex')) {
         return blockOf(parentElement);
       }
       else {
@@ -38,7 +42,7 @@ exports.linksByType = async page => await page.evaluateHandle(() => {
   };
   // Populate it.
   links.forEach(link => {
-    if (isInline(link) && hasAdjacentText(link)) {
+    if (isFluid(link) && hasAdjacentText(link)) {
       linkTypes.inline.push(link);
     }
     else {
