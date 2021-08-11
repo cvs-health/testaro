20894a562a22574e9ac53b1ff47c02ca30e174fa
Jonathan Pool <jonathan.pool@cvshealth.com>
2021-08-11 13:40:25 -0400

    Made bulk stable

--- tests/bulk/app.js
+++ tests/bulk/app.js
@@ -1,5 +1,6 @@
 // Reports the count of visible elements.
 exports.reporter = async page => {
+  await page.waitForSelector('body');
   const visibleElements = await page.$$('body :visible');
   return {result: {visibleElements: visibleElements.length}};
 };
