84ddd48381fb370b4e0900cd03d38e693d1d8e7a
Jonathan Pool <pool@stulta.com>
2021-08-21 16:23:00 -0400

    Debugged

--- procs/test/hover.js
+++ procs/test/hover.js
@@ -1,7 +1,10 @@
 // Finds and marks navigation elements that can be hover-disclosed.
 exports.hover = async (page, withItems) => {
+  // Identify the elements that are likely to trigger disclosures on hover.
   const triggers = await page.$$('body button:visible, body li:visible');
+  // Identify the selectors of active elements likely to be disclosed by a hover.
   const targetSelectors = 'a:visible, button:visible, input:visible, [role=menuitem]:visible';
+  // Initialize the result.
   const data = {
     triggers: 0,
     targets: 0
@@ -12,24 +15,38 @@ exports.hover = async (page, withItems) => {
   // FUNCTION DEFINITION START
   // Recursively finds and reports triggers and targets.
   const find = async triggers => {
+    // If any potential disclosure triggers remain:
     if (triggers.length) {
+      // Identify the first of them.
       const firstTrigger = triggers[0];
+      // Identify its grandparent element.
       const firstGPJSHandle = await page.evaluateHandle(
         firstTrigger => firstTrigger.parentElement.parentElement, firstTrigger
       );
       const firstGP = firstGPJSHandle.asElement();
+      // Identify the visible active descendants of the grandparent.
       const preVisibles = await firstGP.$$(targetSelectors);
       try {
-        await firstTrigger.hover({
-          force: true,
-          timeout: 700
-        });
+        // Hover over the potential trigger.
+        await firstTrigger.hover();
+        // Identify the visible active descendants of the grandparent.
         const postVisibles = await firstGP.$$(targetSelectors);
+        // Hover over the upper-left corner of the page, to undo any current disclosures.
+        await page.hover('body', {
+          position: {
+            x: 0,
+            y: 0
+          }
+        });
+        // If hovering disclosed any element
         if (postVisibles.length > preVisibles.length) {
+          // Increment the counts of triggers and targets.
           data.triggers++;
           const targetCount = postVisibles.length - preVisibles.length;
           data.targets += targetCount;
+          // If details are to be reported:
           if (withItems) {
+            // Report them.
             const triggerDataJSHandle = await page.evaluateHandle(args => {
               const trigger = args[0];
               const preVisibles = args[1];
@@ -38,12 +55,12 @@ exports.hover = async (page, withItems) => {
               .filter(el => ! preVisibles.includes(el))
               .map(el => ({
                 tagName: el.tagName,
-                text: el.textContent || el.outerHTML
+                text: el.textContent.trim() || el.outerHTML
               }));
               return {
                 tagName: trigger.tagName,
                 id: trigger.id || 'NONE',
-                text: trigger.textContent || `{${trigger.outerHTML.slice(0, 100)}}`,
+                text: trigger.textContent.trim() || `{${trigger.outerHTML.slice(0, 100)}}`,
                 newVisibles
               };
             }, [firstTrigger, preVisibles, postVisibles]);
@@ -51,16 +68,16 @@ exports.hover = async (page, withItems) => {
             data.items.push(triggerData);
           }
         }
-        await page.evaluate(
-          trigger => trigger.dispatchEvent(new MouseEvent('mouseout')), firstTrigger
-        );
       }
       catch (error) {
         1;
       }
+      // Process the remaining potential triggers.
       await find(triggers.slice(1));
     }
   };
+  // Find and document the hover-triggered disclosures.
   await find(triggers);
+  // Return the result.
   return data;
 };
