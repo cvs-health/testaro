e47f53cfb34d5c781742ebf534c6bd244d78d62e
Jonathan Pool <pool@stulta.com>
2021-09-07 07:58:19 -0400

    Removed obsolete test

--- index.js
+++ index.js
@@ -65,7 +65,6 @@ const tests = {
   axe: 'conduct and report an Axe test',
   bodyText: 'give the text content of the page body',
   bulk: 'report the count of visible elements',
-  combo: 'report results and a score from multiple tests',
   focOl: 'tabulate and list focusable elements with and without focus outlines',
   focOp: 'tabulate and list visible focusable and operable elements',
   focOpAll: 'tabulate and list focusable and operable elements after making all visible',
