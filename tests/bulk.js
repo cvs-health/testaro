08a68fe7c5e5952c53e4e64ceba5c650e55aca92
Jonathan Pool <pool@stulta.com>
2021-09-19 09:03:42 -0400

    Added bulk proc and bulk test error catch

--- tests/bulk.js
+++ tests/bulk.js
@@ -1,6 +1,10 @@
 // Reports the count of visible elements.
 exports.reporter = async page => {
-  await page.waitForSelector('body');
+  await page.waitForSelector('body', {timeout: 10000})
+  .catch(error => {
+    console.log(`ERROR AWAITING BODY (${error.message})`);
+    return {result: 'BULK TEST TIMED OUT'};
+  });
   const visibleElements = await page.$$('body :visible');
   return {result: {visibleElements: visibleElements.length}};
 };
