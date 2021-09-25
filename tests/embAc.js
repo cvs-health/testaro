8057aa85f3281cea855c85a7c4c44b5c246ba66a
Jonathan Pool <pool@stulta.com>
2021-09-25 08:12:52 -0400

    Debugged

--- tests/embAc.js
+++ tests/embAc.js
@@ -7,25 +7,29 @@ exports.reporter = async (page, withItems) => {
     // Returns a space-minimized copy of a string.
     const compact = string => string.replace(/[\t\n]/g, '').replace(/\s{2,}/g, ' ').trim();
     // FUNCTION DEFINITION END
-    const items = [];
-    // Total and, if requested, itemize the faulty elements.
-    const totals = bads.reduce((tot, current) => {
-      const {tagName} = current;
-      if (tagName === 'A') {
-        tot.links++;
-      }
-      else {
-        tot.buttons++;
-      }
-      if (withItems) {
-        items.push(compact(current.outerHTML));
-      }
-      return tot;
-    }, {
+    const totals = {
       links: 0,
       buttons: 0
+    };
+    const items = [];
+    // Total and, if requested, itemize the faulty elements.
+    bads.forEach(bad => {
+      const {tagName} = bad;
+      if (tagName === 'A') {
+        totals.links++;
+      }
+      else {
+        totals.buttons++;
+      }
+      if (withItems) {
+        items.push(compact(bad.outerHTML));
+      }
     });
     // Return the result.
-    return {result: {totals}};
+    const data = {totals};
+    if (withItems) {
+      data.items = items;
+    }
+    return {result: data};
   }, withItems);
 };
