bf621c04fe6d3a7b3214e5017e25079fd4046968
Jonathan Pool <jonathan.pool@cvshealth.com>
2021-10-15 16:06:49 -0400

    Debugged

--- tests/focInd.js
+++ tests/focInd.js
@@ -5,33 +5,63 @@ exports.reporter = async (page, withItems, revealAll) => {
     await require('../procs/test/allVis').allVis(page);
   }
   // Get data on the focusable visible elements with and without indicators.
-  return {result: await page.$$eval('body *:visible', elements => {
+  return {result: await page.$$eval('body *:visible', (elements, withItems) => {
     // Initialize the data.
-    const data = [];
+    const data = {
+      totals: {
+        total: 0,
+        types: {
+          indicatorMissing: {
+            total: 0,
+            tagNames: {}
+          },
+          indicatorPresent: {
+            total: 0,
+            tagNames: {}
+          }
+        }
+      }
+    };
+    if (withItems) {
+      data.items = {
+        indicatorMissing: [],
+        indicatorPresent: []
+      };
+    }
     elements.forEach(element => {
       if (element.tabIndex === 0) {
+        data.totals.total++;
         const styleBlurred = Object.assign({}, window.getComputedStyle(element));
         element.focus({preventScroll: true});
         const styleFocused = window.getComputedStyle(element);
         const diff = prop => styleFocused[prop] !== styleBlurred[prop];
-        const datum = {
-          indicator: diff('borderStyle')
-            || (styleFocused.borderStyle !== 'none' && diff('borderWidth'))
-            || diff('outlineStyle')
-            || (styleFocused.outlineStyle !== 'none' && diff('outlineWidth'))
-            || diff('fontSize')
-            || diff('fontStyle')
-            || diff('textDecorationLine')
-            || diff('textDecorationStyle')
-            || diff('textDecorationThickness'),
-          tagName: element.tagName
-        };
-        if (withItems) {
-          datum.text = element.textContent.trim().slice(0, 100);
+        const hasInd
+          = diff('borderStyle')
+          || (styleFocused.borderStyle !== 'none' && diff('borderWidth'))
+          || diff('outlineStyle')
+          || (styleFocused.outlineStyle !== 'none' && diff('outlineWidth'))
+          || diff('fontSize')
+          || diff('fontStyle')
+          || diff('textDecorationLine')
+          || diff('textDecorationStyle')
+          || diff('textDecorationThickness');
+        const type = data.totals.types[hasInd ? 'indicatorPresent' : 'indicatorMissing'];
+        type.total++;
+        const tagName = element.tagName;
+        if (type.tagNames[tagName]) {
+          type.tagNames[tagName]++;
         }
-        data.push(datum);
+        else {
+          type.tagNames[tagName] = 1;
+        }
+        if (withItems) {
+          data.items[type].push({
+            tagName,
+            text: element.textContent.trim().replace(/\s{2,}/g, ' ').slice(0, 100)
+          });
+        }
       }
     });
     return data;
-  })};
+  }, withItems)};
 };
