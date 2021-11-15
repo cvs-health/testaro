0984737860aba11ec89053ae52d85ad34f975223
Jonathan Pool <jonathan.pool@cvshealth.com>
2021-11-15 11:04:56 -0500

    Debugged

--- procs/report/scoreAll.sh
+++ procs/report/scoreAll.sh
@@ -1,10 +1,9 @@
-/*
-  scoreAll.sh
-  Performs all reporting operations after a script is executed on a batch.
-  Arguments:
-    0. Subdirectory of the report directory.
-    1. Subdirectory of the docTemplates directory.
-*/
-node ./scoreAgg process.argv[2]
-node ./scoreTable process.argv[2] aut
-node ./scoreDoc process.argv[2] process.argv[3]
+#  scoreAll.sh
+#  Performs all reporting operations after a script is executed on a batch.
+#  Arguments:
+#    0. Subdirectory of the report directory.
+#    1. Subdirectory of the docTemplates directory.
+
+node ./procs/report/scoreAgg process.argv[2]
+node ./procs/report/scoreTable process.argv[2] aut
+node ./procs/report/scoreDoc process.argv[2] process.argv[3]
