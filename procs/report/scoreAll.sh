d3335ab41d849b11fd01e6d1e082d221d8b33c94
Jonathan Pool <jonathan.pool@cvshealth.com>
2021-11-15 11:19:00 -0500

    Debugged

--- procs/report/scoreAll.sh
+++ procs/report/scoreAll.sh
@@ -4,6 +4,7 @@
 #    0. Subdirectory of the report directory.
 #    1. Subdirectory of the docTemplates directory.
 
-node ./procs/report/scoreAgg process.argv[2]
-node ./procs/report/scoreTable process.argv[2] aut
-node ./procs/report/scoreDoc process.argv[2] process.argv[3]
+#!
+node ./procs/report/scoreAgg $1
+node ./procs/report/scoreTable $1 aut
+node ./procs/report/scoreDoc $1 $2
