f31c97c8e717da60196eb344af2e754d3b61fef4
Jonathan Pool <pool@stulta.com>
2022-01-03 14:42:28 -0500

    Debugged

--- tests/alfatest.js
+++ tests/alfatest.js
@@ -7,10 +7,14 @@ const {Scraper} = require('@siteimprove/alfa-scraper');
 const {Rules} = require('@siteimprove/alfa-rules');
 // Conducts and reports an alfa test.
 exports.reporter = async () => {
+  console.log('Starting to run reporter');
   // Get the data on the elements violating the default alfa rules.
   Scraper.with(async scraper => {
+    console.log('Starting for loop');
     for (const input of await scraper.scrape('https://tenon.io/')) {
       console.log(await Audit.of(input, Rules).evaluate());
     }
   });
 };
+exports.reporter();
+
