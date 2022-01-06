56d7d832741dc8582de3e2fa82b59802dee60187
Jonathan Pool <jonathan.pool@cvshealth.com>
2022-01-06 16:31:41 -0500

    Started adding AATT test

--- tests/aatt.js
+++ tests/aatt.js
@@ -2,15 +2,26 @@
   aatt
   This test implements the HTML CodeSniffer ruleset for accessibility via AATT.
 */
+
 // IMPORTS
 const {evaluate} = require('aatt');
+
+// FUNCTIONS
 exports.reporter = async page => {
-  const source = await page.evaluate(() => document.body.innerHTML);
-  const data = await evaluate({
+  // Get the HTML of the document body.
+  const source = await page.content();
+  // Return the result of a test with the HTML CodeSniffer WCAG 2.1 AA ruleset as a string.
+  const report = await evaluate({
     source,
     output: 'json',
     engine: 'htmlcs',
     level: 'WCAG2AA'
   });
-  return {result: data};
+  // Remove the non-JSON prefix from the string.
+  const reportJSON = report.replace(/^.+?\n/, '');
+  // Convert the JSON string to an array.
+  const issueArray = JSON.parse(reportJSON);
+  // Remove the notices from the array.
+  const majorIssues = issueArray.filter(issue => issue.type !== 'notice');
+  return {result: majorIssues};
 };
