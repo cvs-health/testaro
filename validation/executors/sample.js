420952081589306250178c1c045d7f4371a1459c
Jonathan Pool <jonathan.pool@cvshealth.com>
2022-03-02 08:14:18 -0500

    Debugged

--- validation/executors/sample.js
+++ validation/executors/sample.js
@@ -4,7 +4,7 @@
 const options = {
   reports: `${__dirname}/../reports`,
   // To use the sample batch, uncomment the following line.
-  batches: `${__dirname}/validation/batches/sample.json`,
+  batches: `${__dirname}/../batches/sample.json`,
   script: `${__dirname}/../scripts/app/sample.json`
 };
 const {handleRequest} = require('../../index');
