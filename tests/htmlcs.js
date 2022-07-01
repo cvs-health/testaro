26b04f053078f4cf0fa0df4c0ba6314a010e5d62
Jonathan Pool <jonathan.pool@cvshealth.com>
2022-07-01 09:28:56 -0400

    Compacted htmlcs reports and omitted notices

--- tests/htmlcs.js
+++ tests/htmlcs.js
@@ -9,24 +9,37 @@ exports.reporter = async page => {
   await page.addScriptTag({
     path: `${__dirname}/../htmlcs/HTMLCS.js`
   });
-  const messageStrings = [];
+  let messageStrings = [];
   for (const standard of ['WCAG2A', 'WCAG2AA', 'WCAG2AAA']) {
     const nextIssues = await page.evaluate(standard => HTMLCS_RUNNER.run(standard), standard);
     messageStrings.push(... nextIssues);
   }
+  // Sort the issues by class and standard.
   messageStrings.sort();
-  const result = [];
+  // Remove any duplicate issues.
+  messageStrings = [... new Set(messageStrings)];
+  // Initialize the result.
+  const result = {
+    Error: {},
+    Warning: {}
+  };
+  // For each issue:
   messageStrings.forEach(string => {
     const parts = string.split(/\|/g);
     const partCount = parts.length;
     if (partCount !== 6) {
       console.log(`ERROR: Issue string ${string} has too few or too many parts`);
     }
-    else {
-      result.push({
-        issueType: parts[0],
-        standard: parts[1],
-        description: parts[4],
+    // If it is an error or a warning (not a notice):
+    else if (['Error', 'Warning'].includes(parts[0])) {
+      // Add the issue to the result.
+      if (! result[parts[0]][parts[1]]) {
+        result[parts[0]][parts[1]] = {
+          description: parts[4],
+          issues: []
+        };
+      }
+      result[parts[0]][parts[1]].issues.push({
         tagName: parts[2],
         id: parts[3],
         code: parts[5]
