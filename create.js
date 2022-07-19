514a0d6645b5fae885aff2aaebb4354be33b5b89
Jonathan Pool <jonathan.pool@cvshealth.com>
2022-07-19 12:16:31 -0400

    Debugging

--- create.js
+++ create.js
@@ -18,10 +18,17 @@ const {batchify} = require('./batchify');
 const scriptDir = process.env.SCRIPTDIR;
 const batchDir = process.env.BATCHDIR;
 const reportDir = process.env.REPORTDIR;
+const crashHosts = [];
+const timeoutHosts = [];
+
+// ########## VARIABLES
+
+let healthy = true;
+let timeLimit = 300;
 
 // ########## FUNCTIONS
 
-// Runs one script and writes a report file.
+// Runs one script with no batch and writes a report file.
 const runHost = async (id, script) => {
   const report = {
     id,
@@ -34,10 +41,70 @@ const runHost = async (id, script) => {
   const reportJSON = JSON.stringify(report, null, 2);
   await fs.writeFile(`${reportDir}/${id}.json`, reportJSON);
 };
+// Waits.
+const wait = ms => {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve('');
+    }, ms);
+  });
+};
+// Recursively runs host scripts.
+const runHosts = async specs => {
+  // If any scripts remain to be run and the process has not been interrupted:
+  if (specs.length && healthy) {
+    // Run the first one and save the report with a host-suffixed ID.
+    const spec = specs.shift();
+    const {id, host, script} = spec;
+    // Fork a child process to run the script for that host.
+    const subprocess = fork(
+      'runHost', [id, JSON.stringify(script), JSON.stringify(host)],
+      {
+        detached: true,
+        stdio: [0, 1, 2, 'ipc']
+      }
+    );
+    // If the execution continues past the deadline:
+    const timer = setTimeout(async () => {
+      clearTimeout(timer);
+      // Record the host as timed out.
+      timeoutHosts.push(id);
+      // Kill the child process.
+      subprocess.send('interrupt');
+      // Run the remaining host scripts.
+      await runHosts(specs);
+    }, script.timeLimit || timeLimit);
+    // If the child process crashes:
+    subprocess.on('exit', async () => {
+      clearTimeout(timer);
+      // Record the host as crashed.
+      crashHosts.push(id);
+      // Run the remaining host scripts.
+      await runHosts(specs);
+    });
+    // If the child process sends a report:
+    subprocess.on('message', async message => {
+      clearTimeout(timer);
+      // Save it as a file.
+      await fs.writeFile(`${reportDir}/${id}.json`, message);
+      // Run the remaining host scripts.
+      await runHosts(specs);
+    });
+
+    const startTime = Date.now();
+    console.log(`${sizedRep} ${timeStamp}-....json in ${process.env.REPORTDIR}`);
+    if (timeoutHosts.length) {
+      console.log(`Reports not created:\n${JSON.stringify(timeoutHosts), null, 2}`);
+    }
+    if (crashHosts.length) {
+      console.log(
+        `Hosts crashed with or without report:\n${JSON.stringify(crashHosts, null, 2)}`
+      );
+    }
+  }
+};
 // Runs a file-based job and writes a report file for the script or each host.
 exports.runJob = async (scriptID, batchID) => {
-  let healthy = true;
-  let childAlive = true;
   process.on('SIGINT', () => {
     console.log('ERROR: Terminal interrupted runJob');
     healthy = false;
@@ -47,8 +114,7 @@ exports.runJob = async (scriptID, batchID) => {
       const scriptJSON = await fs.readFile(`${scriptDir}/${scriptID}.json`, 'utf8');
       const script = JSON.parse(scriptJSON);
       // Get the time limit of the script or, if none, set it to 5 minutes.
-      let {timeLimit} = script;
-      timeLimit = timeLimit || 300;
+      timeLimit = script.timeLimit || timeLimit;
       // Identify the start time and a timestamp.
       const timeStamp = Math.floor((Date.now() - Date.UTC(2022, 1)) / 2000).toString(36);
       // If there is a batch:
@@ -62,76 +128,6 @@ exports.runJob = async (scriptID, batchID) => {
         const sizedRep = `${batchSize} report${batchSize > 1 ? 's' : ''}`;
         const timeoutHosts = [];
         const crashHosts = [];
-        // FUNCTION DEFINITION START
-        // Recursively runs host scripts.
-        const runHosts = async specs => {
-          // If any scripts remain to be run and the process has not been interrupted:
-          if (specs.length && healthy) {
-            childAlive = true;
-            // Run the first one and save the report with a host-suffixed ID.
-            const spec = specs.shift();
-            const {id, host, script} = spec;
-            const subprocess = fork(
-              'runHost', [id, JSON.stringify(script), JSON.stringify(host)],
-              {
-                detached: true,
-                stdio: [0, 1, 2, 'ipc']
-              }
-            );
-            subprocess.on('exit', () => {
-              childAlive = false;
-            });
-            subprocess.on('message', async message => {
-              await fs.writeFile(`${reportDir}/${id}.json`, message);
-            });
-            const startTime = Date.now();
-            // At 5-second intervals:
-            const reCheck = setInterval(async () => {
-              // If the user has not interrupted the process:
-              if (healthy) {
-                // If there is no need to keep checking:
-                const reportNames = await fs.readdir(reportDir);
-                const timedOut = Date.now() - startTime > 1000 * timeLimit;
-                const reportWritten = reportNames.includes(`${id}.json`);
-                if (timedOut || reportWritten || ! childAlive) {
-                  // Stop checking.
-                  clearInterval(reCheck);
-                  // If the cause is a timeout:
-                  if (timedOut) {
-                    // Add the host to the array of timed-out hosts.
-                    timeoutHosts.push(id);
-                  }
-                  // Otherwise, if the cause is a child crash:
-                  else if (! (childAlive || reportWritten)) {
-                    // Add the host to the array of crashed hosts.
-                    crashHosts.push(id);
-                  }
-                  // Run the script of the next host.
-                  runHosts(specs);
-                }
-              }
-              // Otherwise, i.e. if the user has interrupted the process:
-              else {
-                // Tell the script run to quit.
-                subprocess.send('interrupt');
-                // Stop checking.
-                clearInterval(reCheck);
-              }
-            }, 5000);
-          }
-          else {
-            console.log(`${sizedRep} ${timeStamp}-....json in ${process.env.REPORTDIR}`);
-            if (timeoutHosts.length) {
-              console.log(`Reports not created:\n${JSON.stringify(timeoutHosts), null, 2}`);
-            }
-            if (crashHosts.length) {
-              console.log(
-                `Hosts crashed with or without report:\n${JSON.stringify(crashHosts, null, 2)}`
-              );
-            }
-          }
-        };
-        // FUNCTION DEFINITION END
         // Recursively run each host script and save the reports.
         runHosts(specs);
       }
