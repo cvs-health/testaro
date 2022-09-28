e9f1e3a391ed57cd6bc7c85388c52495db440032
Jonathan Pool <jonathan.pool@cvshealth.com>
2022-09-28 16:57:03 -0400

    Continued developing allHidden validator

--- tests/allHidden.js
+++ tests/allHidden.js
@@ -5,17 +5,17 @@
 exports.reporter = async page => {
   const data = await page.evaluate(() => {
     const {body} = document;
-    const main = body ? body.querySelector('main') : null;
+    const main = body && body.querySelector('main');
     const styles = {
       doc: window.getComputedStyle(document.documentElement),
-      body: body ? window.getComputedStyle(body) : null,
-      main: main ? window.getComputedStyle(main) : null
+      body: body && window.getComputedStyle(body),
+      main: main && window.getComputedStyle(main)
     };
     const data = {
       hidden: {
         document: document.documentElement.hidden,
-        body: body && body.hidden,
-        main: main && main.hidden
+        body: body ? body.hidden : false,
+        main: main ? main.hidden : false
       },
       reallyHidden: {
         document: false,
@@ -23,19 +23,19 @@ exports.reporter = async page => {
         main: false
       },
       display: {
-        document: styles.doc && styles.doc.display,
+        document: styles.doc.display,
         body: styles.body && styles.body.display,
         main: styles.main && styles.main.display
       },
       visHidden: {
-        document: styles.doc && styles.doc.visibility === 'hidden',
-        body: styles.body && styles.body.visibility === 'hidden',
-        main: styles.main && styles.main.visibility === 'hidden'
+        document: styles.doc.visibility === 'hidden',
+        body: styles.body ? styles.body.visibility === 'hidden' : false,
+        main: styles.main ? styles.main.visibility === 'hidden' : false
       },
       ariaHidden: {
         document: document.documentElement.ariaHidden === 'true',
-        body: body && body.ariaHidden === 'true',
-        main: main && main.ariaHidden === 'true'
+        body: body ? body.ariaHidden === 'true' : false,
+        main: main ? main.ariaHidden === 'true' : false
       }
     };
     ['document', 'body', 'main'].forEach(element => {
