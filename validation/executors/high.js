440736e655479c2ae5c57d647f2a083d297ede59
Jonathan Pool <jonathan.pool@cvshealth.com>
2022-11-12 20:05:07 -0500

    Refactored validation jobs and debugged high validator

--- validation/executors/high.js
+++ validation/executors/high.js
@@ -8,46 +8,42 @@ const fs = require('fs/promises');
 // ########## CONSTANTS
 
 const projectRoot = `${__dirname}/../..`;
+process.env.JOBDIR = `${projectRoot}/validation/jobs`;
 process.env.REPORTDIR = `${projectRoot}/temp`;
 const reportDir = process.env.REPORTDIR;
+const jobID = '00000-simple-example';
 
 // ########## OPERATION
 
 // Run the simple job and write a report.
 const {runJob} = require(`${projectRoot}/high`);
-runJob('simple')
+runJob(jobID)
 .then(
   // When the report has been written:
   async () => {
-    // Open it.
-    const fileNames = await fs.readdir(reportDir);
-    const reportNames = fileNames.filter(name => name.endsWith('-simple.json'));
-    if (reportNames.length) {
-      try {
-        // Check its log and act lengths against expectations.
-        const reportJSON = await fs.readFile(`${reportDir}/${reportNames[0]}`);
-        const report = JSON.parse(reportJSON);
-        const {log, acts} = report;
-        if (log.length !== 2) {
-          console.log(
-            `Failure: log length is ${log.length} instead of 2 (see temp/${reportNames[0]}})`
-          );
-        }
-        else if (acts.length !== 3) {
-          console.log(
-            `Failure: acts length is ${acts.length} instead of 3 (see temp/${reportNames[0]}})`
-          );
-        }
-        else {
-          console.log(`Success (report is in temp/${reportNames[0]})`);
-        }
+    try {
+      // Check it against expectations.
+      const reportJSON = await fs.readFile(`${reportDir}/${jobID}.json`);
+      const report = JSON.parse(reportJSON);
+      const {job, acts, jobData} = report;
+      if (! job) {
+        console.log('Failure: Report omits job');
       }
-      catch(error) {
-        console.log(`ERROR: ${error.message}`);
+      else if (acts.length !== 3) {
+        console.log('Failure: Counts of acts is not 3');
+      }
+      else if (! jobData) {
+        console.log('Failure: Report omits jobData');
+      }
+      else if (jobData.endTime < jobData.startTime) {
+        console.log('Failure: End time precedes start time');
+      }
+      else {
+        console.log(`Success (report is in temp/${jobID}.json)`);
       }
     }
-    else {
-      console.log('ERROR: report not found');
+    catch(error) {
+      console.log(`ERROR: ${error.message}`);
     }
   },
   error => {
