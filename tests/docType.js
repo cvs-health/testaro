eab5a0aa274e7040eb4d3b9f5b120efa3488e86b
Jonathan Pool <jonathan.pool@cvshealth.com>
2022-11-16 21:46:17 -0500

    Made docType test stricter and debugged

--- tests/docType.js
+++ tests/docType.js
@@ -1,12 +1,14 @@
 /*
   docType
   Derived from the bbc-a11y allDocumentsMustHaveAW3cRecommendedDoctype test.
-  This test reports a failure to equip the page document with a W3C-recommended doctype.
+  This test reports a failure to equip the page document with a W3C-recommended HTML doctype.
 */
 exports.reporter = async page => {
   // Identify the visible links without href attributes.
-  const docType = await page.evaluate(() => document.doctype);
-  return {result: {
-    docHasType: Boolean(docType)
-  }};
+  const docHasType = await page.evaluate(() => {
+    const docType = document.doctype;
+    const docHasType = !! docType && docType.name && docType.name.toLowerCase() === 'html';
+    return docHasType;
+  });
+  return {result: {docHasType}};
 };
