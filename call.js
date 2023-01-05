e7834710b70e29a12f4b2333e8e27a2da9a601e3
Jonathan Pool <jonathan.pool@cvshealth.com>
2023-01-05 10:42:52 -0500

    Refactored and further commented call

--- call.js
+++ call.js
@@ -32,24 +32,29 @@ const reportDir = process.env.REPORTDIR;
 
 // Fulfills a testing request.
 const callRun = async jobIDStart => {
-  // Get the job.
+  // Find the job.
   const jobDirFileNames = await fs.readdir(toDoDir);
   const jobFileName = jobDirFileNames.find(fileName => fileName.startsWith(jobIDStart));
+  // If it exists:
   if (jobFileName) {
+    // Get it.
     const jobJSON = await fs.readFile(`${toDoDir}/${jobFileName}`, 'utf8');
     const job = JSON.parse(jobJSON);
+    // Run it.
     await doJob(job);
     console.log(`Job completed and report ${job.id}.json saved in ${reportDir}/raw`);
   }
+  // Otherwise, i.e. if the job does not exist.
   else {
+    // Report the error.
     console.log(`ERROR: No to-do job ID starts with ${jobIDStart}`);
   }
 };
 // Starts a watch.
 const callWatch = async (isDirWatch, isForever, interval) => {
-  console.log(
-    `Starting a ${isForever === 'true' ? 'repeating' : 'one-time'} ${isDirWatch === 'true' ? 'directory' : 'network'} watch`
-  );
+  const whenType = isForever === 'true' ? 'repeating' : 'one-time';
+  const whereType = isDirWatch === 'true' ? 'directory' : 'network';
+  console.log(`Starting ${whenType} ${whereType} watch`);
   await cycle(isDirWatch === 'true', isForever === 'true', Number.parseInt(interval, 10));
 };
 
