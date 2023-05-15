2352a5cbcd01490e6f1fc832f93ea9785482524a
Jonathan Pool <pool@jpdev.pro>
2023-05-14 20:49:13 -0500

    Made spawned directory watches repeating

--- dirWatch.js
+++ dirWatch.js
@@ -10,23 +10,33 @@ const {spawn} = require('node:child_process');
 
 // ########## CONSTANTS
 
-const interval = 30;
+const interval = process.argv(2);
 
 // ########## FUNCTIONS
 
-// Spawns a directory watch.
+// Spawns a one-time directory watch.
 const spawnWatch = (command, args) => spawn(command, args, {stdio: ['inherit', 'inherit', 'pipe']});
+// Repeatedly spawns a one-time directory watch.
+const reWatch = () => {
+  const watcher = spawnWatch('node', ['call', 'watch', 'true', 'false', interval]);
+  let error = '';
+  watcher.stderr.on('data', data => {
+    error += data.toString();
+  });
+  watcher.on('close', async code => {
+    if (error) {
+      console.log(`ERROR: ${error.slice(0, 100)}`);
+    }
+    if (code === 0) {
+      console.log('Watcher exited successfully');
+    }
+    else {
+      console.log(`Watcher exited with error code ${code}`);
+    }
+    reWatch();
+  });
+};
 
 // ########## OPERATION
 
-const watcher = spawnWatch('node', ['call', 'watch', 'true', 'false', '20']);
-let error = '';
-watcher.stderr.on('data', data => {
-  error += data.toString();
-});
-watcher.on('close', code => {
-  if (error) {
-    console.log(`ERROR: ${error.slice(0, 100)}`);
-  }
-  console.log(`Watcher exited with code ${code}`);
-});
+reWatch();
