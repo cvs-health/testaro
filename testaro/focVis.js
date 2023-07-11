9b30eb91fb0a62987a99a8c3ec4bab3e2e3834f8
Jonathan Pool <jonathan.pool@cvshealth.com>
2023-07-11 08:52:13 -0500

    Converted focVis to locators

--- testaro/focVis.js
+++ testaro/focVis.js
@@ -3,6 +3,14 @@
   Derived from the bbc-a11y elementsMustBeVisibleOnFocus test.
   This test reports links that are off the display when focused.
 */
+
+// ########## IMPORTS
+
+// Module to get locator data.
+const {getLocatorData} = require('../procs/getLocatorData');
+
+// ########## FUNCTIONS
+
 exports.reporter = async (page, withItems) => {
   // Get a locator for the initially visible links.
   const locAll = page.locator('a:visible');
@@ -20,59 +28,52 @@ exports.reporter = async (page, withItems) => {
       locs.push(loc);
     }
   }
+  // Initialize the standard result.
+  const data = {};
+  const totals = [0, 0, 0, 0];
+  const standardInstances = [];
   // For each off-display link:
   for (const loc of locs) {
     // Get data on it.
     const elData = await getLocatorData(loc);
-  }
-
-  const badLinks = await page.$$eval('a:visible', links => {
-    // FUNCTION DEFINITION START
-    // Returns a space-minimized copy of a string.
-    const compact = string => string.replace(/[\t\n]/g, '').replace(/\s{2,}/g, ' ').trim();
-    // FUNCTION DEFINITION END
-    const badLinks = [];
-    links.forEach(link => {
-      link.focus();
-      if (link.offsetTop + link.offsetHeight <= 0 || link.offsetLeft + link.offsetWidth <= 0) {
-        badLinks.push({
-          id: link.id,
-          text: compact(link.textContent)
-        });
+    // Add to the totals.
+    totals[2]++;
+    if (withItems) {
+      let where;
+      if (elData.location.type === 'selector') {
+        where = 'above or to the left of';
       }
-    });
-    return badLinks;
-  });
-  const data = {
-    total: badLinks.length
-  };
-  if (withItems) {
-    data.items = badLinks;
-  }
-  const totals = [0, 0, data.total, 0];
-  const standardInstances = [];
-  if (data.items) {
-    data.items.forEach(item => {
+      else {
+        const isLeft = elData.location.spec.x < 0;
+        const isAbove = elData.location.spec.y < 0;
+        if (isLeft && isAbove) {
+          where = 'above and to the left of';
+        }
+        else if (isLeft) {
+          where = 'to the left of';
+        }
+        else if (isAbove) {
+          where = 'above';
+        }
+        else {
+          where = 'possibly above or to the left of';
+        }
+      }
       standardInstances.push({
         ruleID: 'focVis',
-        what: 'Visible link is above or to the left of the display',
+        what: `Visible link is ${where} the display`,
         ordinalSeverity: 2,
         tagName: 'A',
-        id: item.id,
-        location: {
-          doc: '',
-          type: '',
-          spec: ''
-        },
-        excerpt: item.text
+        id: elData.id,
+        location: elData.location,
+        excerpt: elData.excerpt
       });
-    });
+    }
   }
-  else if (data.total) {
+  if (! withItems) {
     standardInstances.push({
       ruleID: 'focVis',
       what: 'Visible links are above or to the left of the display',
-      count: data.total,
       ordinalSeverity: 2,
       tagName: 'A',
       id: '',
