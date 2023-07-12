1cf36b45be6faf382e1c3df275215102e970e0f0
Jonathan Pool <jonathan.pool@cvshealth.com>
2023-07-11 21:06:01 -0500

    Continued locator conversion

--- testaro/linkTitle.js
+++ testaro/linkTitle.js
@@ -3,55 +3,50 @@
   Related to Tenon rule 79.
   This test reports links with title attributes with values contained in the link text contents.
 */
+
+// ########## IMPORTS
+
+// Module to get locator data.
+const {getLocatorData} = require('../procs/getLocatorData');
+
+// ########## FUNCTIONS
+
 exports.reporter = async (page, withItems) => {
-  // Identify the links with target=_blank attributes.
-  const badLinkData = await page.$$eval(
-    'a[title]',
-    titleLinks => {
-      // FUNCTION DEFINITIONS START
-      // Returns a space-minimized copy of a string.
-      const compact = string => string.replace(/[\t\n]/g, ' ').replace(/\s{2,}/g, ' ').trim();
-      const lcCompact = string => compact(string).toLowerCase();
-      // FUNCTION DEFINITIONS END
-      const badLinks = titleLinks.filter(link => {
-        const compactText = lcCompact(link.textContent);
-        const compactTitle = lcCompact(link.title);
-        return compactText.includes(compactTitle);
-      });
-      return badLinks.map(badLink => ({
-        id: badLink.id || '',
-        text: compact(badLink.textContent)
-      }));
-    }
-  );
-  const data = {
-    total: badLinkData.length
-  };
-  const totals = [data.total, 0, 0, 0];
+  const data = {};
+  const totals = [0, 0, 0, 0];
   const standardInstances = [];
-  if (withItems) {
-    data.items = badLinkData;
-    data.items.forEach(item => {
-      standardInstances.push({
-        ruleID: 'linkTitle',
-        what: 'Link has a title attribute that repeats link text content',
-        ordinalSeverity: 0,
-        tagName: 'A',
-        id: item.id,
-        location: {
-          doc: '',
-          type: '',
-          spec: ''
-        },
-        excerpt: item.text
-      });
-    });
+  // Get locators for the links with title attributes.
+  const locAll = page.locator('a[title]');
+  const locs = await locAll.all();
+  // For each of them:
+  for (const loc of locs) {
+    // Get data on it.
+    const title = await loc.getAttribute('title');
+    const elData = await getLocatorData(loc);
+    // If the title value is contained in the excerpt:
+    if (elData.excerpt.toLowerCase().includes(title.toLowerCase())) {
+      // Add to the result.
+      totals[0]++;
+      if (withItems) {
+        standardInstances.push({
+          ruleID: 'linkTitle',
+          what: 'Link has a title attribute that repeats link text content',
+          ordinalSeverity: 0,
+          tagName: 'A',
+          id: elData.id,
+          location: elData.location,
+          excerpt: elData.excerpt
+        });
+      }
+    }
   }
-  else if (data.total) {
+  // If itemization is not required and there are any instances:
+  if (! withItems && totals[0]) {
+    // Add a summary instance to the result.
     standardInstances.push({
       ruleID: 'linkTitle',
       what: 'Links have title attributes that repeat link text contents',
-      count: data.total,
+      count: totals[0],
       ordinalSeverity: 0,
       tagName: 'A',
       id: '',
