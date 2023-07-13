529d19d6b219fe94489f304b4628c260c48f6e9f
Jonathan Pool <jonathan.pool@cvshealth.com>
2023-07-13 09:54:22 -0500

    Converted targetSize to locators

--- testaro/targetSize.js
+++ testaro/targetSize.js
@@ -3,97 +3,78 @@
   Related to Tenon rule 152, but stricter.
   This test reports buttons, inputs, and non-inline links with widths or heights smaller than 44 pixels.
 */
+
+// ########## IMPORTS
+
+// Module to get locator data.
+const {getLocatorData} = require('../procs/getLocatorData');
+// Module to classify links.
+const {isInlineLink} = require('../procs/isInlineLink');
+
+// ########## FUNCTIONS
+
 exports.reporter = async (page, withItems) => {
-  // Identify the buttons, inputs, and non-inline links smaller than 44 pixels.
-  const linkTypesJSHandle = await require('../procs/linksByType').linksByType(page);
-  const data = await page.$$eval(
-    // Identify all buttons, inputs, and links.
-    'button, input',
-    (suspects, linkTypesJSHandle) => {
-      // Initialize the result.
-      const data = {
-        total: 0,
-        items: []
-      };
-      // FUNCTION DEFINITIONS START
-      // Returns a space-minimized copy of a string.
-      const compact = string => string.replace(/[\t\n]/g, '').replace(/\s{2,}/g, ' ').trim();
-      const textsOf = element => {
-        const textContent = element.textContent;
-        const ariaLabel = element.ariaLabel;
-        const labeler = element.labels
-          ? Array.from(element.labels).map(label => label.textContent).join(' ')
-          : '';
-        const refLabelID = element.getAttribute('aria-labelledby');
-        const refLabel = refLabelID ? document.getElementById(refLabelID).textContent : '';
-        const texts = [];
-        [textContent, ariaLabel, labeler, refLabel].forEach(textType => {
-          if (textType) {
-            const compactTextType = compact(textType);
-            if (compactTextType) {
-              texts.push(compactTextType);
-            }
-          }
-        });
-        return texts.join(' ');
-      };
-      // FUNCTION DEFINITIONS END
-      // Identify the buttons, inputs, and non-inline links.
-      const eligibles = suspects.concat(linkTypesJSHandle.list);
-      // For each of them:
-      eligibles.forEach(eligible => {
-        // If its size is substandard:
-        const width = eligible.offsetWidth;
-        const height = eligible.offsetHeight;
-        if (width < 44 || height < 44) {
-          console.log('Adding to result');
-          // Add it to the result.
-          data.total++;
-          data.items.push({
-            width: width,
-            height: height,
-            tagName: eligible.tagName,
-            id: eligible.id || '',
-            text: textsOf(eligible)
-          });
-        }
-      });
-      return data;
-    }, linkTypesJSHandle
-  );
-  // Initialize the standard result.
-  const totals = [data.total, 0, 0, 0];
+  // Initialize the result.
+  const data = {};
+  const totals = [0, 0, 0, 0];
   const standardInstances = [];
-  // If itemization was requested:
-  if (withItems) {
-    // Add it to the standard result.
-    data.items.forEach(item => {
-      standardInstances.push({
-        ruleID: 'targetSize',
-        what:
-        `Interactive element has a substandard size (${item.width} px wide, ${item.height} px high)`,
-        ordinalSeverity: 0,
-        tagName: item.tagName,
-        id: item.id,
-        location: {
-          doc: '',
-          type: '',
-          spec: ''
-        },
-        excerpt: item.text
-      });
-    });
+  // Get locators for all eligible elements.
+  const nonlinkLocAll = page.locator('button, input');
+  const nonlinkLocsAll = await nonlinkLocAll.all();
+  const linkLocAll = page.locator('a');
+  const linkLocsAll = await linkLocAll.all();
+  const locs = nonlinkLocsAll;
+  for (const loc of linkLocsAll) {
+    if (! await isInlineLink(loc)) {
+      locs.push(loc);
+    }
   }
-  // Otherwise, i.e. if itemization was not requested:
-  else {
-    // Delete the items from the result.
-    data.items = [];
-    // Add a summary instance to the standard result.
+  // For each of them:
+  for (const loc of locs) {
+    // Get whether it violates the rule, and, if so, how.
+    const howBad = await loc.evaluate(element => {
+      const width = element.offsetWidth;
+      const height = element.offsetHeight;
+      if (width < 44 || height < 44) {
+        return {
+          width,
+          height
+        };
+      }
+      else {
+        return null;
+      }
+    });
+    // If so:
+    if (howBad) {
+      // Add to the totals.
+      totals[0]++;
+      // If itemization is required:
+      if (withItems) {
+        // Get data on the element.
+        const elData = await getLocatorData(loc);
+        // Add an instance to the result.
+        standardInstances.push({
+          ruleID: 'targetSize',
+          what:
+          `Interactive element is only ${howBad.width} pixels wide and ${howBad.height} pixels high`,
+          ordinalSeverity: 0,
+          tagName: elData.tagName,
+          id: elData.id,
+          location: elData.location,
+          excerpt: elData.excerpt
+        });
+      }
+    }
+  }
+  // If itemization is not required:
+  if (! withItems) {
+    // Add a summary instance to the result.
     standardInstances.push({
       ruleID: 'targetSize',
-      what: 'Interactive elements are smaller than 44 px wide and high',
+      what: 'Interactive elements are smaller than 44 pixels wide and high',
       ordinalSeverity: 0,
-      count: data.total,
+      count: totals[0],
       tagName: '',
       id: '',
       location: {
@@ -104,7 +85,7 @@ exports.reporter = async (page, withItems) => {
       excerpt: ''
     });
   }
-  // Return the result and standard result.
+  // Return the result.
   return {
     data,
     totals,
