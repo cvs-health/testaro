a9430b05248bd776888840b99ad544f305e9ef6d
Jonathan Pool <jonathan.pool@cvshealth.com>
2023-07-18 12:46:03 -0400

    Debugged

--- procs/getLocatorData.js
+++ procs/getLocatorData.js
@@ -1,78 +1,85 @@
 // Returns data about the element identified by a locator.
 exports.getLocatorData = async loc => {
-  // Get the facts obtainable from the browser.
-  const data = await loc.evaluate(element => {
-    // Tag name.
-    const tagName = element.tagName;
-    // ID.
-    const id = element.id || '';
-    // Texts.
-    const {textContent} = element;
-    const alts = Array.from(element.querySelectorAll('img[alt]:not([alt=""])'));
-    const altTexts = alts.map(alt => alt.getAttribute('alt'));
-    const altsText = altTexts.join(' ');
-    const ariaLabelText = element.ariaLabel || '';
-    const refLabelID = element.getAttribute('aria-labelledby');
-    const refLabel = refLabelID ? document.getElementById(refLabelID) : '';
-    const refLabelText = refLabel ? refLabel.textContent : '';
-    let labelsText = '';
-    if (tagName === 'INPUT') {
-      const labels = element.labels;
-      const labelTexts = [];
-      labels.forEach(label => {
-        labelTexts.push(label.textContent);
-      });
-      labelsText = labelTexts.join(' ');
+  const locCount = await loc.count();
+  if (locCount === 1) {
+    // Get the facts obtainable from the browser.
+    const data = await loc.evaluate(element => {
+      // Tag name.
+      const tagName = element.tagName;
+      // ID.
+      const id = element.id || '';
+      // Texts.
+      const {textContent} = element;
+      const alts = Array.from(element.querySelectorAll('img[alt]:not([alt=""])'));
+      const altTexts = alts.map(alt => alt.getAttribute('alt'));
+      const altsText = altTexts.join(' ');
+      const ariaLabelText = element.ariaLabel || '';
+      const refLabelID = element.getAttribute('aria-labelledby');
+      const refLabel = refLabelID ? document.getElementById(refLabelID) : '';
+      const refLabelText = refLabel ? refLabel.textContent : '';
+      let labelsText = '';
+      if (tagName === 'INPUT') {
+        const labels = element.labels;
+        const labelTexts = [];
+        labels.forEach(label => {
+          labelTexts.push(label.textContent);
+        });
+        labelsText = labelTexts.join(' ');
+      }
+      let text = [textContent, altsText, ariaLabelText, refLabelText, labelsText]
+      .join(' ')
+      .replace(/\s+/g, ' ')
+      .trim();
+      if (! text) {
+        text = element.outerHTML.replace(/\s+/g, ' ').trim();
+      }
+      // Location.
+      let location = {
+        doc: 'dom',
+        type: 'box',
+        spec: {}
+      };
+      if (id) {
+        location.type = 'selector';
+        location.spec = `#${id}`;
+      }
+      // Return the data.
+      return {
+        tagName,
+        id,
+        location,
+        excerpt: text
+      };
+    });
+    // If an ID-based selector could not be defined:
+    if (data.location.type === 'box') {
+      // Define a bounding-box-based location.
+      const rawSpec = await loc.boundingBox();
+      // If there is a bounding box:
+      if (rawSpec) {
+        // Populate the location.
+        Object.keys(rawSpec).forEach(specName => {
+          data.location.spec[specName] = Math.round(rawSpec[specName]);
+        });
+      }
+      // Otherwise, i.e. if there is no bounding box:
+      else {
+        // Empty the location.
+        data.location.doc = '';
+        data.location.type = '';
+        data.location.spec = '';
+      }
     }
-    let text = [textContent, altsText, ariaLabelText, refLabelText, labelsText]
-    .join(' ')
-    .replace(/\s+/g, ' ')
-    .trim();
-    if (! text) {
-      text = element.outerHTML.replace(/\s+/g, ' ').trim();
-    }
-    // Location.
-    let location = {
-      doc: 'dom',
-      type: 'box',
-      spec: {}
-    };
-    if (id) {
-      location.type = 'selector';
-      location.spec = `#${id}`;
+    // If the text is long:
+    if (data.excerpt.length > 400) {
+      // Truncate its middle.
+      data.excerpt = `${data.excerpt.slice(0, 200)} … ${data.excerpt.slice(-200)}`;
     }
     // Return the data.
-    return {
-      tagName,
-      id,
-      location,
-      excerpt: text
-    };
-  });
-  // If an ID-based selector could not be defined:
-  if (data.location.type === 'box') {
-    // Define a bounding-box-based location.
-    const rawSpec = await loc.boundingBox();
-    // If there is a bounding box:
-    if (rawSpec) {
-      // Populate the location.
-      Object.keys(rawSpec).forEach(specName => {
-        data.location.spec[specName] = Math.round(rawSpec[specName]);
-      });
-    }
-    // Otherwise, i.e. if there is no bounding box:
-    else {
-      // Empty the location.
-      data.location.doc = '';
-      data.location.type = '';
-      data.location.spec = '';
-    }
+    return data;
   }
-  // If the text is long:
-  if (data.excerpt.length > 400) {
-    // Truncate its middle.
-    data.excerpt = `${data.excerpt.slice(0, 200)} … ${data.excerpt.slice(-200)}`;
+  else {
+    console.log(`ERROR: Locator count to get data from is ${locCount} instead of 1`);
+    return null;
   }
-  // Return the data.
-  return data;
 };
--- testaro/buttonMenu.js
+++ testaro/buttonMenu.js
@@ -24,34 +24,47 @@ const getAdjacentIndexWithWrap = (groupSize, startIndex, increment) => {
 };
 // Returns whether a key created the expected effective focus.
 const focusSuccess = async (miLocAll, priorIndex, key, isPseudo) => {
+  console.log(`isPseudo in focusSuccess is ${isPseudo}`);
+  // Initialize the result.
+  const result = {
+    isOK: false,
+    newFocIndex: null
+  };
   // Get the index of the effectively focused menu item.
-  const focData = await miLocAll.evaluateAll(elements => {
+  const focData = await miLocAll.evaluateAll((elements, isPseudo) => {
+    // Get the currently focused element.
     let focEl = document.activeElement;
+    // If the menu manages pseudofocus:
     let miFocIndex;
     if (isPseudo) {
+      // Get the index of the effectively focused menu item.
       const pseudoFocID = focEl.getAttribute('aria-activedescendant');
       if (pseudoFocID) {
         miFocIndex = elements.map(element => element.id).indexOf(pseudoFocID);
       }
     }
+    // Otherwise, i.e. if the menu manages true focus:
     else {
+      // Get the index of the focused menu item.
       miFocIndex = elements.indexOf(focEl);
     }
+    // If an effectively focused menu item was identified:
     if (miFocIndex > -1) {
+      // Return data on the menu items.
       return {
         count: elements.length,
         miFocIndex
       };
     }
+    // Otherwise, i.e. if no effectively focused menu item was identified:
     else {
+      // Return this.
       return null;
     }
-  });
-  let result = {
-    isOK: false,
-    newFocIndex: null
-  };
+  }, isPseudo);
+  // If data on the menu items were obtained:
   if (focData) {
+    // Get whether the effective focus is as expected and update the result.
     if (key === 'Home' && focData.miFocIndex === 0) {
       result.isOK = true;
       result.newFocIndex = 0;
@@ -75,6 +88,8 @@ const focusSuccess = async (miLocAll, priorIndex, key, isPseudo) => {
       }
     }
   }
+  // Return the result.
+  return result;
 };
 // Performs the test and reports the result.
 exports.reporter = async (page, withItems) => {
@@ -87,93 +102,125 @@ exports.reporter = async (page, withItems) => {
     'button[aria-controls][aria-expanded=false][aria-haspopup=true], button[aria-controls][aria-expanded=false][aria-haspopup=menu]'
   );
   const mbLocsAll = await mbLocAll.all();
-  // For each menu button:
-  for (const mbLoc of mbLocsAll) {
-    // Get a locator for its menu.
-    const menuID = await mbLoc.getAttribute('aria-controls');
-    const menuLoc = mbLoc.locator(`[id=${menuID}][role=menu], [id=${menuID}][role=menubar]`);
-    // If the button controls a menu:
-    if (menuLoc) {
-      // Get data on the menu.
-      const elData = await getLocatorData(menuLoc);
-      // Get the orientation of the menu.
-      let orientation;
-      try {
-        orientation = await menuLoc.getAttribute('aria-orientation', {timeout: 100});
-      }
-      catch(error) {
-        console.log(`About to get role of:\n${JSON.stringify(elData, null, 2)}`);
-        const menuRole = await menuLoc.getAttribute('role');
-        orientation = menuRole === 'menubar' ? 'horizontal' : 'vertical';
-      }
-      // Get the focus-management type of the menu.
-      let isPseudo;
-      menuLoc.getAttribute('aria-activedescendant', {timeout: 100})
-      .then(() => {
-        isPseudo = true;
-      })
-      .catch(() => {
-        isPseudo = false;
-      });
-      console.log('Got isPseudo');
-      // Get locators for its direct menu items.
-      const miLocAll = menuLoc.locator(
-        ':scope [role=menuitem]:not([role=menu] [role=menuitem]):not([role=menubar] [role=menuitem])'
-      );
-      console.log('Got miLocAll');
-      const miLocsAll = await miLocAll.all();
-      // If there are at least 2 of them:
-      if (miLocsAll.length > 1) {
-        // Focus the menu button and press the key that opens its menu from the top.
-        await mbLoc.press(orientation === 'horizontal' ? 'ArrowRight' : 'ArrowDown');
-        // Randomly draw 12 keys from Home, End, and the applicable arrow keys.
-        const keys = ['Home', 'End'];
-        if (orientation === 'horizontal') {
-          keys.push('ArrowLeft', 'ArrowRight');
-        }
-        else {
-          keys.push('ArrowUp', 'ArrowDown');
-        }
-        const trialKeys = [];
-        let trialKeyCount = 0;
-        while (trialKeyCount++ < 12) {
-          trialKeys.push(keys[Math.floor(4 * Math.random())]);
-        }
-        let focIndex = 0;
-        // For each key:
-        for (const key of trialKeys) {
-          // Press it.
-          await page.keyboard.press(key);
-          // Get whether the expected focus occurred.
-          const focData = await focusSuccess(miLocAll, focIndex, key, isPseudo);
-          // If so:
-          if (focData.isOK) {
-            // Update the index of the effective focus.
-            focIndex = focData.newFocIndex;
-          }
-          // Otherwise, i.e. if the expected focus did not occur:
-          else {
-            // Add to the totals.
-            totals[2]++;
-            // If itemization is required:
-            if (withItems) {
-              // Add an instance to the result.
-              standardInstances.push({
-                ruleID: 'buttonMenu',
-                what: `Menu responds nonstandardly to the ${key} key`,
-                ordinalSeverity: 2,
-                tagName: elData.tagName,
-                id: elData.id,
-                location: elData.location,
-                excerpt: elData.excerpt
-              });
+  console.log(`Menu button count: ${mbLocsAll.length}`);
+  // Processes a menu button.
+  const mbDo = async mbIndex => {
+    // Get locators for all menu buttons.
+    const mbLocsAll = await mbLocAll.all();
+    // Get the locator for the one with the index.
+    const mbLoc = mbLocsAll[mbIndex];
+    // If it exists:
+    if (mbLoc) {
+      // Get a locator for its menu.
+      const mbLocID = await mbLoc.getAttribute('id');
+      console.log(`About to get menu ID for button ${mbLocID}`);
+      const menuID = await mbLoc.getAttribute('aria-controls');
+      console.log(`Menu ID is ${menuID}`);
+      const menuLoc = page.locator(`[id=${menuID}][role=menu], [id=${menuID}][role=menubar]`);
+      // If the button controls a menu:
+      if (menuLoc) {
+        // Get data on the menu.
+        const elData = await getLocatorData(menuLoc);
+        // If data were obtained:
+        if (elData) {
+          // Get the orientation and focus-management type of the menu.
+          const extraData = await menuLoc.evaluate(element => {
+            const extraData = {};
+            let orientation = element.getAttribute('aria-orientation');
+            if (! orientation) {
+              const role = element.getAttribute('role');
+              if (role === 'menubar') {
+                orientation = 'horizontal';
+              }
+              else if (role === 'menu') {
+                orientation = 'vertical';
+              }
+              else {
+                orientation = null;
+              }
             }
-            break;
+            extraData.orientation = orientation;
+            const isPseudo = !! element.getAttribute('aria-activedescendant');
+            extraData.isPseudo = isPseudo;
+            return extraData;
+          });
+          // If they were obtained:
+          if (extraData) {
+            // Get locators for its direct menu items.
+            const miLocAll = menuLoc.locator(
+              ':scope [role=menuitem]:not([role=menu] [role=menuitem]):not([role=menubar] [role=menuitem])'
+            );
+            const miLocsAll = await miLocAll.all();
+            // If there are at least 2 of them:
+            if (miLocsAll.length > 1) {
+              // Focus the menu button and press the key that opens its menu from the top.
+              await mbLoc.press(extraData.orientation === 'horizontal' ? 'ArrowRight' : 'ArrowDown');
+              // Randomly draw 12 keys from Home, End, and the applicable arrow keys.
+              const keys = ['Home', 'End'];
+              if (extraData.orientation === 'horizontal') {
+                keys.push('ArrowLeft', 'ArrowRight');
+              }
+              else {
+                keys.push('ArrowUp', 'ArrowDown');
+              }
+              const trialKeys = [];
+              let trialKeyCount = 0;
+              while (trialKeyCount++ < 12) {
+                trialKeys.push(keys[Math.floor(4 * Math.random())]);
+              }
+              let focIndex = 0;
+              // For each key:
+              for (const key of trialKeys) {
+                // Press it.
+                await page.keyboard.press(key);
+                // Get whether the expected focus occurred.
+                console.log(`isPseudo 1 is ${extraData.isPseudo}`);
+                const focData = await focusSuccess(miLocAll, focIndex, key, extraData.isPseudo);
+                // If so:
+                if (focData.isOK) {
+                  // Update the index of the effective focus.
+                  focIndex = focData.newFocIndex;
+                }
+                // Otherwise, i.e. if the expected focus did not occur:
+                else {
+                  // Add to the totals.
+                  totals[2]++;
+                  // If itemization is required:
+                  if (withItems) {
+                    // Add an instance to the result.
+                    standardInstances.push({
+                      ruleID: 'buttonMenu',
+                      what: `Menu responds nonstandardly to the ${key} key`,
+                      ordinalSeverity: 2,
+                      tagName: elData.tagName,
+                      id: elData.id,
+                      location: elData.location,
+                      excerpt: elData.excerpt
+                    });
+                  }
+                  break;
+                }
+              }
+            }
           }
         }
       }
+      // Otherwise, i.e. if the button does not control a menu:
+      else {
+        // Report this.
+        console.log('ERROR: Button does not control a menu');
+      }
+      // Process the next menu button.
+      await mbDo(mbIndex + 1);
     }
-  }
+    // Otherwise, i.e. if it does not exist:
+    else {
+      // Return this.
+      return false;
+    }
+  };
+  // Process the menu buttons.
+  await mbDo(0);
   // If itemization is not required and there are any instances:
   if (! withItems && totals[2]) {
     // Add a summary instance to the result.
