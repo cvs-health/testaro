189f60afd058ecd64aeb5d00ca266eb817be10e6
Jonathan Pool <jonathan.pool@cvshealth.com>
2023-07-18 17:57:29 -0400

    Finished buttonMenu conversion and debugging

--- testaro/buttonMenu.js
+++ testaro/buttonMenu.js
@@ -1,7 +1,11 @@
 /*
   buttonMenu
   This test reports nonstandard navigation among menu items of button-controlled menus.
-  Standards are based on https://www.w3.org/TR/wai-aria-practices-1.1/#menu.
+  Standards are based on https://www.w3.org/TR/wai-aria-practices-1.1/#menu. The trialKeys argument
+  is an array of strings, each of which may be 'Home', 'End', '+', or '-'. The '+' string represents
+  the ArrowDown or ArrowRight key, and the '-' string represents the ArrowUp or ArrowLeft key,
+  depending on the orientation of the current menu. When the trialKeys argument is missing or is an
+  empty array, 12 keys are selected at random.
 */
 
 // ########## IMPORTS
@@ -24,7 +28,6 @@ const getAdjacentIndexWithWrap = (groupSize, startIndex, increment) => {
 };
 // Returns whether a key created the expected effective focus.
 const focusSuccess = async (miLocAll, priorIndex, key, isPseudo) => {
-  console.log(`isPseudo in focusSuccess is ${isPseudo}`);
   // Initialize the result.
   const result = {
     isOK: false,
@@ -92,69 +95,82 @@ const focusSuccess = async (miLocAll, priorIndex, key, isPseudo) => {
   return result;
 };
 // Performs the test and reports the result.
-exports.reporter = async (page, withItems) => {
+exports.reporter = async (page, withItems, trialKeySpecs = []) => {
   // Initialize the result.
   const data = {};
   const totals = [0, 0, 0, 0];
   const standardInstances = [];
-  // Get locators for all menu buttons with collapsed menus.
+  // Get locators for all menu buttons.
   const mbLocAll = page.locator(
-    'button[aria-controls][aria-expanded=false][aria-haspopup=true], button[aria-controls][aria-expanded=false][aria-haspopup=menu]'
+    'button[aria-controls][aria-expanded][aria-haspopup=true], button[aria-controls][aria-expanded][aria-haspopup=menu]'
   );
   const mbLocsAll = await mbLocAll.all();
-  console.log(`Menu button count: ${mbLocsAll.length}`);
-  // Processes a menu button.
-  const mbDo = async mbIndex => {
-    // Get locators for all menu buttons.
-    const mbLocsAll = await mbLocAll.all();
-    // Get the locator for the one with the index.
-    const mbLoc = mbLocsAll[mbIndex];
-    // If it exists:
-    if (mbLoc) {
-      // Get a locator for its menu.
-      const mbLocID = await mbLoc.getAttribute('id');
-      console.log(`About to get menu ID for button ${mbLocID}`);
-      const menuID = await mbLoc.getAttribute('aria-controls');
-      console.log(`Menu ID is ${menuID}`);
-      const menuLoc = page.locator(`[id=${menuID}][role=menu], [id=${menuID}][role=menubar]`);
-      // If the button controls a menu:
-      if (menuLoc) {
-        // Get data on the menu.
-        const elData = await getLocatorData(menuLoc);
-        // If data were obtained:
-        if (elData) {
-          // Get the orientation and focus-management type of the menu.
-          const extraData = await menuLoc.evaluate(element => {
-            const extraData = {};
-            let orientation = element.getAttribute('aria-orientation');
-            if (! orientation) {
-              const role = element.getAttribute('role');
-              if (role === 'menubar') {
-                orientation = 'horizontal';
-              }
-              else if (role === 'menu') {
-                orientation = 'vertical';
-              }
-              else {
-                orientation = null;
-              }
+  for (const mbLoc of mbLocsAll) {
+    // Get a locator for its menu.
+    const menuID = await mbLoc.getAttribute('aria-controls');
+    const menuLoc = page.locator(`[id=${menuID}][role=menu], [id=${menuID}][role=menubar]`);
+    // If the button controls a menu:
+    if (menuLoc) {
+      // Get data on the menu.
+      const elData = await getLocatorData(menuLoc);
+      // If data were obtained:
+      if (elData) {
+        // Get the orientation and focus-management type of the menu.
+        const extraData = await menuLoc.evaluate(element => {
+          const extraData = {};
+          let orientation = element.getAttribute('aria-orientation');
+          if (! orientation) {
+            const role = element.getAttribute('role');
+            if (role === 'menubar') {
+              orientation = 'horizontal';
             }
-            extraData.orientation = orientation;
-            const isPseudo = !! element.getAttribute('aria-activedescendant');
-            extraData.isPseudo = isPseudo;
-            return extraData;
-          });
-          // If they were obtained:
-          if (extraData) {
-            // Get locators for its direct menu items.
-            const miLocAll = menuLoc.locator(
-              ':scope [role=menuitem]:not([role=menu] [role=menuitem]):not([role=menubar] [role=menuitem])'
-            );
-            const miLocsAll = await miLocAll.all();
-            // If there are at least 2 of them:
-            if (miLocsAll.length > 1) {
-              // Focus the menu button and press the key that opens its menu from the top.
-              await mbLoc.press(extraData.orientation === 'horizontal' ? 'ArrowRight' : 'ArrowDown');
+            else if (role === 'menu') {
+              orientation = 'vertical';
+            }
+            else {
+              orientation = null;
+            }
+          }
+          extraData.orientation = orientation;
+          const isPseudo = !! element.getAttribute('aria-activedescendant');
+          extraData.isPseudo = isPseudo;
+          return extraData;
+        });
+        // If they were obtained:
+        if (extraData) {
+          // Get locators for its direct menu items.
+          const miLocAll = menuLoc.locator(
+            ':scope [role=menuitem]:not([role=menu] [role=menuitem]):not([role=menubar] [role=menuitem])'
+          );
+          const miLocsAll = await miLocAll.all();
+          // If there are at least 2 of them:
+          if (miLocsAll.length > 1) {
+            // Ensure that the menu is collapsed.
+            await menuLoc.evaluate(element => {
+              element.setAttribute('aria-expanded', false);
+            });
+            // Focus the menu button and press the key that opens its menu from the top.
+            await mbLoc.press(extraData.orientation === 'horizontal' ? 'ArrowRight' : 'ArrowDown');
+            // If trial keys have been specified:
+            const trialKeys = [];
+            if (trialKeySpecs.length) {
+              // Implement them.
+              trialKeySpecs.forEach(spec => {
+                if (['Home', 'End'].includes(spec)) {
+                  trialKeys.push(spec);
+                }
+                else if (spec === '-') {
+                  trialKeys.push(extraData.orientation === 'horizontal' ? 'ArrowLeft' : 'ArrowUp');
+                }
+                else if (spec === '+') {
+                  trialKeys.push(
+                    extraData.orientation === 'horizontal' ? 'ArrowtRight' : 'ArrowDown'
+                  );
+                }
+              });
+            }
+            // Otherwise, i.e. if trial keys have not been specified:
+            else {
               // Randomly draw 12 keys from Home, End, and the applicable arrow keys.
               const keys = ['Home', 'End'];
               if (extraData.orientation === 'horizontal') {
@@ -163,70 +179,71 @@ exports.reporter = async (page, withItems) => {
               else {
                 keys.push('ArrowUp', 'ArrowDown');
               }
-              const trialKeys = [];
               let trialKeyCount = 0;
               while (trialKeyCount++ < 12) {
                 trialKeys.push(keys[Math.floor(4 * Math.random())]);
               }
-              let focIndex = 0;
-              // For each key:
-              for (const key of trialKeys) {
-                // Press it.
-                await page.keyboard.press(key);
-                // Get whether the expected focus occurred.
-                console.log(`isPseudo 1 is ${extraData.isPseudo}`);
-                const focData = await focusSuccess(miLocAll, focIndex, key, extraData.isPseudo);
-                // If so:
-                if (focData.isOK) {
-                  // Update the index of the effective focus.
-                  focIndex = focData.newFocIndex;
+            }
+            // Add the list of trial keys to the result.
+            data.trialKeys = trialKeys;
+            let focIndex = 0;
+            // For each key in the trial:
+            for (const key of trialKeys) {
+              // Press it.
+              await page.keyboard.press(key);
+              // Get whether the expected focus occurred.
+              const focData = await focusSuccess(miLocAll, focIndex, key, extraData.isPseudo);
+              // If so:
+              if (focData.isOK) {
+                // Update the index of the effective focus.
+                focIndex = focData.newFocIndex;
+              }
+              // Otherwise, i.e. if the expected focus did not occur:
+              else {
+                // Add to the totals.
+                totals[2]++;
+                // If itemization is required:
+                if (withItems) {
+                  // Add an instance to the result.
+                  standardInstances.push({
+                    ruleID: 'buttonMenu',
+                    what: `Menu responds nonstandardly to the ${key} key`,
+                    ordinalSeverity: 2,
+                    tagName: elData.tagName,
+                    id: elData.id,
+                    location: elData.location,
+                    excerpt: elData.excerpt
+                  });
                 }
-                // Otherwise, i.e. if the expected focus did not occur:
-                else {
-                  // Add to the totals.
-                  totals[2]++;
-                  // If itemization is required:
-                  if (withItems) {
-                    // Add an instance to the result.
-                    standardInstances.push({
-                      ruleID: 'buttonMenu',
-                      what: `Menu responds nonstandardly to the ${key} key`,
-                      ordinalSeverity: 2,
-                      tagName: elData.tagName,
-                      id: elData.id,
-                      location: elData.location,
-                      excerpt: elData.excerpt
-                    });
-                  }
-                  break;
-                }
+                break;
               }
             }
           }
         }
+        // Otherwise, i.e. if the orientation and focus-management type were not obtained:
+        else {
+          // Report this.
+          console.log('ERROR: Menu orientation and focus-management type not obtained');
+        }
       }
-      // Otherwise, i.e. if the button does not control a menu:
+      // Otherwise, i.e. if the data were not obtained:
       else {
         // Report this.
-        console.log('ERROR: Button does not control a menu');
+        console.log('ERROR: Menu data not obtained');
       }
-      // Process the next menu button.
-      await mbDo(mbIndex + 1);
     }
-    // Otherwise, i.e. if it does not exist:
+    // Otherwise, i.e. if the menu was not identified:
     else {
-      // Return this.
-      return false;
+      // Report this.
+      console.log('ERROR: Menu controlled by menu button not found');
     }
-  };
-  // Process the menu buttons.
-  await mbDo(0);
+  }
   // If itemization is not required and there are any instances:
   if (! withItems && totals[2]) {
     // Add a summary instance to the result.
     standardInstances.push({
-      ruleID: 'menuNav',
-      what: 'Menu items respond nonstandardly to navigation keys',
+      ruleID: 'buttonMenu',
+      what: 'Menus respond nonstandardly to navigation keys',
       count: totals[2],
       ordinalSeverity: 2,
       tagName: '',
