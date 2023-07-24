9ebbdbb2688143d9ab0b6fe897513b17c365f841
Jonathan Pool <jonathan.pool@cvshealth.com>
2023-07-24 17:26:28 -0400

    Debugged

--- testaro/hover.js
+++ testaro/hover.js
@@ -33,12 +33,10 @@
 
 // IMPORTS
 
+// Module to get locator data.
+const {getLocatorData} = require('../procs/getLocatorData');
 // Module to get pixel changes between two times.
 const {visChange} = require('../procs/visChange');
-
-// VARIABLES
-
-let hasTimedOut = false;
 
 // FUNCTIONS
 
@@ -65,224 +63,6 @@ const getSample = (population, sampleSize) => {
     return population.map((trigger, index) => index);
   }
 };
-// Returns the impacts of hovering over a sampled trigger.
-const getImpacts = async (
-  interval, triesLeft, root, page, preDescendants, preOpacities
-) => {
-  // If the allowed trial count has not yet been exhausted:
-  if (triesLeft-- && ! hasTimedOut) {
-    // Get the collection of descendants of the root.
-    const postDescendants = await root.$$(':visible');
-    // Identify the prior descendants of the root still in existence.
-    const remainerIndexes = await page.evaluate(args => {
-      const preDescendants = args[0];
-      const postDescendants = args[1];
-      const remainerIndexes = preDescendants
-      .map((element, index) => postDescendants.includes(element) ? index : -1)
-      .filter(index => index > -1);
-      return remainerIndexes;
-    }, [preDescendants, postDescendants]);
-    // Get the impacts of the hover event.
-    const additions = postDescendants.length - remainerIndexes.length;
-    const removals = preDescendants.length - remainerIndexes.length;
-    const remainers = [];
-    for (const index of remainerIndexes) {
-      remainers.push({
-        element: preDescendants[index],
-        preOpacity: preOpacities[index],
-        postOpacity: await page.evaluate(
-          element => window.getComputedStyle(element).opacity, preDescendants[index]
-        )
-      });
-    }
-    const opacityChangers = remainers
-    .filter(remainer => remainer.postOpacity !== remainer.preOpacity);
-    const opacityImpact = opacityChangers
-      ? await page.evaluate(changers => changers.reduce(
-        (total, current) => total + current.element.querySelectorAll('*').length, 0
-      ), opacityChangers)
-      : 0;
-    // If there are any impacts:
-    if (additions || removals || opacityChangers.length) {
-      // Return them.
-      return {
-        additions,
-        removals,
-        opacityChanges: opacityChangers.length,
-        opacityImpact
-      };
-    }
-    // Otherwise, i.e. if there are no impacts:
-    else {
-      // Try again.
-      return await new Promise(resolve => {
-        setTimeout(() => {
-          resolve(getImpacts(interval, triesLeft, root, page, preDescendants, preOpacities));
-        }, interval);
-      });
-    }
-  }
-  // Otherwise, i.e. if the allowed trial count has been exhausted:
-  else {
-    // Report non-impact.
-    return null;
-  }
-};
-// Recursively adds estimated and itemized impacts of hovering over triggers to data.
-const find = async (data, withItems, page, sample) => {
-  // If any triggers remain and the test has not timed out:
-  if (sample.length && ! hasTimedOut) {
-    // Get and report the impacts until and unless the test times out.
-    try {
-      // Identify the first trigger.
-      const firstTrigger = sample[0];
-      const tagNameJSHandle = await firstTrigger.getProperty('tagName')
-      .catch(() => '');
-      if (tagNameJSHandle) {
-        const tagName = await tagNameJSHandle.jsonValue();
-        // Identify the root of a subtree likely to contain impacted elements.
-        let root = firstTrigger;
-        if (['A', 'BUTTON', 'LI'].includes(tagName)) {
-          const rootJSHandle = await page.evaluateHandle(
-            trigger => {
-              const parent = trigger.parentElement || trigger;
-              const grandparent = parent.parentElement || parent;
-              const greatGrandparent = grandparent.parentElement || parent;
-              return trigger.tagName === 'LI' ? grandparent : greatGrandparent;
-            },
-            firstTrigger
-          );
-          root = rootJSHandle.asElement();
-        }
-        // Identify all the visible descendants of the root.
-        const preDescendants = await root.$$(':visible');
-        // Identify their opacities.
-        const preOpacities = await page.evaluate(elements => elements.map(
-          element => window.getComputedStyle(element).opacity
-        ), preDescendants);
-        // Get the style properties of the trigger.
-        const triggerPreStyles = await getHoverStyles(page, firstTrigger);
-        const multiplier = data.sampling.triggers / data.sampling.triggerSample;
-        const itemData = {
-          tagName,
-          id: (await firstTrigger.getAttribute('id')) || '',
-          text: await textOf(firstTrigger, 100)
-        };
-        try {
-          // Hover over the trigger.
-          await firstTrigger.hover({
-            timeout: 500,
-            noWaitAfter: true
-          });
-          // Repeatedly seek impacts of the hover at intervals.
-          const impacts = await getImpacts(
-            300, 4, root, page, preDescendants, preOpacities, firstTrigger
-          );
-          // Get the style properties of the trigger.
-          const triggerPostStyles = await getHoverStyles(page, firstTrigger);
-          // Add cursor and other style defects to the data.
-          const cursor = triggerPreStyles.cursor;
-          // If the trigger has no cursor:
-          if (cursor === 'none') {
-            // Add this fact to the data.
-            data.totals.noCursors += multiplier;
-            if (withItems) {
-              data.items.noCursors.push(itemData);
-            }
-          }
-          // If the trigger has an improper cursor:
-          if (
-            tagName === 'A' && cursor !== 'pointer'
-            || tagName === 'BUTTON' && cursor !== 'default'
-          ){
-            // Add this fact to the data.
-            data.totals.badCursors += multiplier;
-            if (withItems) {
-              data.items.badCursors.push(itemData);
-            }
-          }
-          // If hover indication is illicit but is present:
-          if (
-            tagName === 'LI'
-            && JSON.stringify(triggerPostStyles) !== JSON.stringify(triggerPreStyles)
-          ) {
-            // Add this fact to the data.
-            data.totals.badIndicators += multiplier;
-            if (withItems) {
-              data.items.badIndicators.push(itemData);
-            }
-          }
-          // If there were any impacts:
-          if (impacts) {
-            // Hover over the upper-left corner of the page, to undo any impacts.
-            await page.hover('body', {
-              position: {
-                x: 0,
-                y: 0
-              },
-              timeout: 500,
-              force: true,
-              noWaitAfter: true
-            });
-            // Wait for any delayed and/or slowed reaction.
-            await page.waitForTimeout(200);
-            await root.waitForElementState('stable');
-            // Increment the estimated counts of triggers and impacts.
-            const {additions, removals, opacityChanges, opacityImpact} = impacts;
-            if (hasTimedOut) {
-              return Promise.resolve('');
-            }
-            else {
-              data.totals.impactTriggers += multiplier;
-              data.totals.additions += additions * multiplier;
-              data.totals.removals += removals * multiplier;
-              data.totals.opacityChanges += opacityChanges * multiplier;
-              data.totals.opacityImpact += opacityImpact * multiplier;
-              // If details are to be reported:
-              if (withItems) {
-                // Add them to the data.
-                data.items.impactTriggers.push({
-                  tagName,
-                  id: itemData.id,
-                  text: await textOf(firstTrigger, 100),
-                  additions,
-                  removals,
-                  opacityChanges,
-                  opacityImpact
-                });
-              }
-            }
-          }
-        }
-        catch (error) {
-          console.log(`ERROR hovering (${error.message.replace(/\n.+/s, '')})`);
-          if (hasTimedOut) {
-            return Promise.resolve('');
-          }
-          else {
-            data.totals.unhoverables += multiplier;
-            if (withItems) {
-              try {
-                data.items.unhoverables.push(itemData);
-              }
-              catch(error) {
-                console.log('ERROR itemizing unhoverable element');
-              }
-            }
-          }
-        }
-      }
-      // Process the remaining potential triggers.
-      await find(data, withItems, page, sample.slice(1));
-    }
-    catch(error) {
-      console.log(`ERROR: Test quit when remaining sample size was ${sample.length}`);
-    }
-  }
-  else {
-    return Promise.resolve('');
-  }
-};
 // Performs the hover test and reports results.
 exports.reporter = async (page, withItems, sampleSize = 20) => {
   // Initialize the result.
@@ -305,13 +85,12 @@ exports.reporter = async (page, withItems, sampleSize = 20) => {
     console.log(
       `ERROR: hover test on sample of ${sample.length} triggers timed out at ${timeLimit} seconds; page closed`
     );
-    hasTimedOut = true;
     data.prevented = true;
     data.error = 'ERROR: hover test timed out';
     clearTimeout(timeout);
   }, 1000 * timeLimit);
   // For each trigger in the sample:
-  const successCount = 0;
+  let successCount = 0;
   for (const loc of sample) {
     // Hover over it and get the fractional pixel change.
     const hoverData = await visChange(page, {
