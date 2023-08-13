d151caa4a7ef266b69c61829adbe36e406d6d1b3
Jonathan Pool <jonathan.pool@cvshealth.com>
2023-08-13 14:20:21 -0400

    Started refactoring autocomplete

--- testaro/autocomplete-old.js
+++ testaro/autocomplete-old.js
@@ -5,57 +5,10 @@
 
 // ########## IMPORTS
 
-// Module to perform common operations.
-const {init, report} = require('../procs/testaro');
 // Module to get locator data.
 const {getLocatorData} = require('../procs/getLocatorData');
 
 // ########## FUNCTIONS
-
-// Runs the test and returns the result.
-exports.reporter = async (
-  page,
-  withItems,
-  givenLabels = ['first name', 'forename', 'given name'],
-  familyLabels = ['last name', 'surname', 'family name'],
-  emailLabels = ['email']
-) => {
-  // Initialize the locators and result.
-  const all = await init(page, 'input[type=text], input[type=email], input:not([type])');
-  // For each locator:
-  const autoValues = {
-    'given-name': givenLabels,
-    'family-name': familyLabels,
-    'email': emailLabels
-  };
-  for (const loc of all.allLocs) {
-    // Get which autocomplete value, if any, its element needs.
-    const data = await getLocatorData(loc);
-    const lcText = data.excerpt.toLowerCase();
-    const neededAutos = Object.keys(autoValues)
-    .filter(autoValue => autoValues[autoValue].some(typeLabel => lcText.includes(typeLabel)));
-    let neededAuto;
-    if (neededAutos.length === 1) {
-      neededAuto = neededAutos[0];
-    }
-    else if (! neededAutos.length && await loc.getAttribute('type') === 'email') {
-      neededAuto = 'email';
-    }
-    // If it needs one:
-    if (neededAuto) {
-      // Get whether it has the one it needs.
-      const actualAuto = await loc.getAttribute('autocomplete');
-      const isBad = actualAuto !== neededAuto;
-      if (isBad) {
-        // Add the locator to the array of violators.
-        all.locs.push(loc);
-      }
-    }
-  }
-  // Populate and return the result.
-  const whats = ['Itemized description', 'Summary description'];
-  return await report(withItems, all, 'ruleID', whats, 0);
-};
 
 // Runs the test and returns the results.
 exports.reporter = async (
