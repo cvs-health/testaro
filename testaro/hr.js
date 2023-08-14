04fe7d0ba31bfa7f133f8f5c903d12b2a6899b67
Jonathan Pool <jonathan.pool@cvshealth.com>
2023-08-14 16:04:06 -0400

    Refactored hr test

--- testaro/hr.js
+++ testaro/hr.js
@@ -7,61 +7,20 @@
 
 // ########## IMPORTS
 
-// Module to get locator data.
-const {getLocatorData} = require('../procs/getLocatorData');
+// Module to perform common operations.
+const {init, report} = require('../procs/testaro');
 
 // ########## FUNCTIONS
 
-// Performs the test.
+// Runs the test and returns the result.
 exports.reporter = async (page, withItems) => {
-  // Initialize the standard result.
-  const data = {};
-  const totals = [0, 0, 0, 0];
-  const standardInstances = [];
-  // Get locators for all applicable elements.
-  const locAll = page.locator('body hr');
-  const locsAll = await locAll.all();
-  // For each of them:
-  for (const loc of locsAll) {
-    // Add to the totals.
-    totals[0]++;
-    // If itemization is required:
-    if (withItems) {
-      // Get data on the element.
-      const elData = await getLocatorData(loc);
-      // Add a standard instance.
-      standardInstances.push({
-        ruleID: 'hr',
-        what: 'Element instead of styling is used for vertical segmentation',
-        ordinalSeverity: 0,
-        tagName: 'HR',
-        id: elData.id,
-        location: elData.location,
-        excerpt: elData.excerpt
-      });
-    }
-  }
-  // If itemization is not required and there are instances:
-  if (! withItems && totals[0]) {
-    // Add a summary instance.
-    standardInstances.push({
-      ruleID: 'hr',
-      what: 'Elements instead of styling are used for vertical segmentation',
-      ordinalSeverity: 0,
-      count: totals[0],
-      tagName: 'HR',
-      id: '',
-      location: {
-        doc: '',
-        type: '',
-        spec: ''
-      },
-      excerpt: ''
-    });
-  }
-  return {
-    data,
-    totals,
-    standardInstances
-  };
+  // Initialize the locators and result.
+  const all = await init(page, 'body hr');
+  all.locs = all.allLocs;
+  // Populate and return the result.
+  const whats = [
+    'Element instead of styling is used for vertical segmentation',
+    'Elements instead of styling are used for vertical segmentation'
+  ];
+  return await report(withItems, all, 'hr', whats, 0, 'HR');
 };
