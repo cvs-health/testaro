dedff20976dd8751325a9158901d9ac62159bd7a
Jonathan Pool <jonathan.pool@cvshealth.com>
2023-08-14 18:18:43 -0400

    Refactored linkUl test

--- procs/isInlineLink.js
+++ procs/isInlineLink.js
@@ -7,17 +7,14 @@
 exports.isInlineLink = async loc => await loc.evaluate(element => {
   // Initialize the link as inline.
   let result = true;
-  console.log('Initialized as inline');
   // If it is in a list item in a list of at least 2 links:
   const listAncestor = element.closest('ul, ol');
   if (listAncestor) {
-    console.log('Has a list ancestor');
     if (listAncestor.children.length > 1 && Array.from(listAncestor.children).every(child => {
       const isValidListItem = child.tagName === 'LI';
       const has1Link = child.querySelectorAll('a').length === 1;
       return isValidListItem && has1Link;
     })) {
-      console.log('List is pure');
       // If the list text is entirely link text:
       const listText = listAncestor.textContent.replace(/\s/g, '');
       let linkTextRaw = '';
@@ -27,7 +24,6 @@ exports.isInlineLink = async loc => await loc.evaluate(element => {
       const linkText = linkTextRaw.replace(/\s/g, '');
       console.log(linkText);
       if (listText === linkText && listText.length) {
-        console.log('No extraneous text, so non-inline');
         // Reclassify the link as non-inline.
         result = false;
       }
--- testaro/linkUl.js
+++ testaro/linkUl.js
@@ -8,82 +8,36 @@
   merely to discover which passages are links.
 */
 
-// ########## IMPORTS
-
-// Module to get locator data.
-const {getLocatorData} = require('../procs/getLocatorData');
+// Module to perform common operations.
+const {init, report} = require('../procs/testaro');
 // Module to classify links.
 const {isInlineLink} = require('../procs/isInlineLink');
 
 // ########## FUNCTIONS
 
+// Runs the test and returns the result.
 exports.reporter = async (page, withItems) => {
-  // Initialize the result.
-  const data = {};
-  const totals = [0, 0, 0, 0];
-  const standardInstances = [];
-  // Get locators for all inline links.
-  const locAll = page.locator('a');
-  const locsAll = await locAll.all();
-  const inlineLocs = [];
-  for (const loc of locsAll) {
-    if (await isInlineLink(loc)) {
-      inlineLocs.push(loc);
-    }
-  }
-  // Get locators for all non-underlined inline links.
-  const locs = [];
-  for (const loc of inlineLocs) {
-    const isUnderlined = await loc.evaluate(element => {
-      const styleDec = window.getComputedStyle(element);
+  // Initialize the locators and result.
+  const all = await init(page, 'a');
+  // For each locator:
+  for (const loc of all.allLocs) {
+    // Get whether its element is underlined.
+    const isUnderlined = await loc.evaluate(el => {
+      const styleDec = window.getComputedStyle(el);
       return styleDec.textDecorationLine === 'underline';
     });
+    // If it is not:
     if (! isUnderlined) {
-      locs.push(loc);
+      // Get whether it is inline.
+      const isInline = await isInlineLink(loc);
+      // If it is:
+      if (isInline) {
+        // Add the locator to the array of violators.
+        all.locs.push(loc);
+      }
     }
   }
-  // Add to the totals.
-  totals[1] = locs.length;
-  // If itemization is required:
-  if (withItems) {
-    // For each non-underlined inline link:
-    for (const loc of locs) {
-      // Get data on it.
-      const elData = await getLocatorData(loc);
-      // Add an instance to the result.
-      standardInstances.push({
-        ruleID: 'linkUl',
-        what: 'Link is inline but has no underline',
-        ordinalSeverity: 1,
-        tagName: 'A',
-        id: elData.id,
-        location: elData.location,
-        excerpt: elData.excerpt
-      });
-    }
-  }
-  // Otherwise, i.e. if itemization is not required:
-  else {
-    // Add a summary instance to the result.
-    standardInstances.push({
-      ruleID: 'linkUl',
-      what: 'Inline links are missing underlines',
-      count: totals[1],
-      ordinalSeverity: 1,
-      tagName: 'A',
-      id: '',
-      location: {
-        doc: '',
-        type: '',
-        spec: ''
-      },
-      excerpt: ''
-    });
-  }
-  // Return the result.
-  return {
-    data,
-    totals,
-    standardInstances
-  };
+  // Populate and return the result.
+  const whats = ['Link is inline but has no underline', 'Inline links are missing underlines'];
+  return await report(withItems, all, 'linkUl', whats, 1);
 };
