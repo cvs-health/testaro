064965f1cd02f368a48a65c2419557ee932c8383
Jonathan Pool <jonathan.pool@cvshealth.com>
2023-08-14 10:25:32 -0400

    Refactored embAc test

--- testaro/embAc.js
+++ testaro/embAc.js
@@ -8,59 +8,37 @@
 
 // ########## IMPORTS
 
-// Module to get locator data.
-const {getLocatorData} = require('../procs/getLocatorData');
+// Module to perform common operations.
+const {init, report} = require('../procs/testaro');
 
 // ########## FUNCTIONS
 
+// Runs the test and returns the result.
 exports.reporter = async (page, withItems) => {
-  // Get locators for all misembedded elements.
-  const loc = page.locator(
-    'a a, a button, a input, a select, button a, button button, button input, button select'
+  // Initialize the locators and result.
+  const all = await init(
+    page, 'a a, a button, a input, a select, button a, button button, button input, button select'
   );
-  const locs = await loc.all();
-  // Initialize the result.
-  const data = {};
-  const totals = [0, 0, 0, 0];
-  const standardInstances = [];
   // For each locator:
-  for (const loc of locs) {
-    // Get data on its element.
-    const parentTagName = await loc.evaluate(element => element.parentElement.tagName);
-    const elData = await getLocatorData(loc);
-    // Add data to the standard result.
-    totals[2]++;
-    if (withItems) {
-      standardInstances.push({
-        ruleID: 'embAc',
-        what: `Interactive element is embedded in a ${parentTagName === 'A' ? 'link' : 'button'}`,
-        ordinalSeverity: 2,
-        tagName: elData.tagName,
-        id: elData.id,
-        location: elData.location,
-        excerpt: elData.excerpt
-      });
-    }
-  };
-  if (! withItems) {
-    standardInstances.push({
-      ruleID: 'embAc',
-      what: 'Interactive elements are contained by links or buttons',
-      ordinalSeverity: 2,
-      count: totals[2],
-      tagName: '',
-      id: '',
-      location: {
-        doc: '',
-        type: '',
-        spec: ''
-      },
-      excerpt: ''
+  for (const loc of all.allLocs) {
+    // Get whether its embedder is a link or a button.
+    const embedderTagName = await loc.evaluate(element => {
+      const embedder = element.parentElement.closest('a, button');
+      return embedder ? embedder.tagName : '';
     });
+    let param = 'a link or button';
+    if (embedderTagName === 'A') {
+      param = 'a link';
+    }
+    else if (embedderTagName === 'BUTTON') {
+      param = 'a button';
+    }
+    all.locs.push([loc, param]);
   }
-  return {
-    data,
-    totals,
-    standardInstances
-  };
+  // Populate and return the result.
+  const whats = [
+    'Interactive element is embedded in __param__',
+    'Interactive elements are contained by links or buttons'
+  ];
+  return await report(withItems, all, 'embAc', whats, 2);
 };
