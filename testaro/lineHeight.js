d9c5f5dee67d2c8932b58f5939805610237ebd9c
Jonathan Pool <jonathan.pool@cvshealth.com>
2023-08-14 17:04:47 -0400

    Refactored lineHeight test

--- testaro/lineHeight.js
+++ testaro/lineHeight.js
@@ -7,98 +7,37 @@
   their subtrees are excluded.
 */
 
-// ########## IMPORTS
-
-// Module to get locator data.
-const {getLocatorData} = require('../procs/getLocatorData');
+// Module to perform common operations.
+const {init, report} = require('../procs/testaro');
 
 // ########## FUNCTIONS
 
-// Gets the ratio of line height to font size not reached, as text, from an ordinal severity.
-const getRatio = ordinalSeverity => ['1.5', '1.3', '1.1', '0.9'][ordinalSeverity];
+// Runs the test and returns the result.
 exports.reporter = async (page, withItems) => {
-  // Initialize the standard result.
-  const data = {};
-  const totals = [0, 0, 0, 0];
-  const standardInstances = [];
-  // Get locators for all body elements with any descendant non-spacing text.
-  const locAll = page.locator('body *', {hasText: /[^\s]/});
-  const locsAll = await locAll.all();
-  // For each of them:
-  for (const loc of locsAll) {
-    // Get data on it.
-    const facts = await loc.evaluate(element => {
-      const styleDec = window.getComputedStyle(element);
+  // Initialize the locators and result.
+  const all = await init(page, 'body *', {hasText: /[^\s]/});
+  // For each locator:
+  for (const loc of all.allLocs) {
+    // Get whether its element violates the rule.
+    const data = await loc.evaluate(el => {
+      const styleDec = window.getComputedStyle(el);
       const {fontSize, lineHeight} = styleDec;
       return {
         fontSize: Number.parseFloat(fontSize),
         lineHeight: Number.parseFloat(lineHeight)
       };
     });
-    // If its line height is substandard:
-    const ratio = facts.lineHeight / facts.fontSize;
-    let ordinalSeverity = -1;
-    if (ratio < 0.9) {
-      ordinalSeverity = 3;
-    }
-    else if (ratio < 1.1) {
-      ordinalSeverity = 2;
-    }
-    else if (ratio < 1.3) {
-      ordinalSeverity = 1;
-    }
-    else if (ratio < 1.5) {
-      ordinalSeverity = 0;
-    }
-    if (ordinalSeverity > -1) {
-      // Add to the totals.
-      totals[ordinalSeverity]++;
-      // If itemization is required:
-      if (withItems) {
-        // Get data on the element.
-        const elData = await getLocatorData(loc);
-        // Add a standard instance.
-        standardInstances.push({
-          ruleID: 'lineHeight',
-          what:
-          `Element line height ${facts.lineHeight} px is less than ${getRatio(ordinalSeverity)} times its font size ${facts.fontSize} px`,
-          ordinalSeverity,
-          tagName: elData.tagName,
-          id: elData.id,
-          location: elData.location,
-          excerpt: elData.excerpt
-        });
-      }
+    // If it does:
+    const isBad = data.lineHeight < 1.5 * data.fontSize;
+    if (isBad) {
+      // Add the locator to the array of violators.
+      all.locs.push([loc, `font size ${data.fontSize} px, line height ${data.lineHeight} px`]);
     }
   }
-  // If itemization is not required:
-  if (! withItems) {
-    // For each ordinal severity:
-    [0, 1, 2, 3].forEach(ordinalSeverity => {
-      // If there are instances with it:
-      if (totals[ordinalSeverity]) {
-        // Add a summary instance.
-        standardInstances.push({
-          ruleID: 'lineHeight',
-          what:
-          `Elements have line heights less than ${getRatio(ordinalSeverity)} times their font sizes`,
-          ordinalSeverity,
-          count: totals[ordinalSeverity],
-          tagName: '',
-          id: '',
-          location: {
-            doc: '',
-            type: '',
-            spec: ''
-          },
-          excerpt: ''
-        });
-      }
-    });
-  }
-  return {
-    data,
-    totals,
-    standardInstances
-  };
+  // Populate and return the result.
+  const whats = [
+    'Element line height is less than 1.5 times its font size (__param__)',
+    'Elements have line heights less than 1.5 times their font sizes'
+  ];
+  return await report(withItems, all, 'lineHeight', whats, 1);
 };
