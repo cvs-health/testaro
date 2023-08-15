5f16521fbcdac3fae20ec2603242f1fdf92b81a4
Jonathan Pool <jonathan.pool@cvshealth.com>
2023-08-15 13:51:08 -0400

    Refactored titledEl test and updated version

--- testaro/titledEl.js
+++ testaro/titledEl.js
@@ -6,61 +6,19 @@
 
 // ########## IMPORTS
 
-// Module to get locator data.
-const {getLocatorData} = require('../procs/getLocatorData');
+// Module to perform common operations.
+const {init, report} = require('../procs/testaro');
 
 // ########## FUNCTIONS
 
+// Runs the test and returns the result.
 exports.reporter = async (page, withItems) => {
-  // Initialize the result.
-  const data = {};
-  const standardInstances = [];
-  // Get locators for inappropriate elements with title attributes.
-  const locAll = page.locator('[title]:not(input, button, textarea, select, iframe):visible');
-  const locs = await locAll.all();
-  // For each of them:
-  for (const loc of locs) {
-    // If itemization is required:
-    if (withItems) {
-      // Get data on the element.
-      const elData = await getLocatorData(loc);
-      const rawTitle = await loc.getAttribute('title');
-      const title = rawTitle.replace(/\s+/g, ' ').slice(0, 50);
-      // Add an instance to the result.
-      standardInstances.push({
-        ruleID: 'titledEl',
-        what: `Ineligible element has a title (${title})`,
-        ordinalSeverity: 2,
-        tagName: elData.tagName,
-        id: elData.id,
-        location: elData.location,
-        excerpt: elData.excerpt
-      });
-    }
-  }
-  // Define the totals.
-  const totals = [0, 0, locs.length, 0];
-  // If itemization is not required:
-  if (! withItems) {
-    // Add a summary instance to the result.
-    standardInstances.push({
-      ruleID: 'titledEl',
-      what: 'Ineligible elements have title attributes',
-      ordinalSeverity: 2,
-      count: totals[2],
-      tagName: '',
-      id: '',
-      location: {
-        doc: '',
-        type: '',
-        spec: ''
-      },
-      excerpt: ''
-    });
-  }
-  return {
-    data,
-    totals,
-    standardInstances
-  };
+  // Initialize the locators and result.
+  const all = await init(page, '[title]:not(input, button, textarea, select, iframe):visible');
+  all.locs = all.allLocs;
+  // Populate and return the result.
+  const whats = [
+    'Ineligible element has a title attribute', 'Ineligible elements have title attributes'
+  ];
+  return await report(withItems, all, 'titledEl', whats, 2);
 };
