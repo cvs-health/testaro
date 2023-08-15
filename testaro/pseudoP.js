8f27bf926df018e4c8da1671c5c3b2f84a5dee8c
Jonathan Pool <jonathan.pool@cvshealth.com>
2023-08-15 06:33:38 -0400

    Refactored pseudoP test

--- testaro/pseudoP.js
+++ testaro/pseudoP.js
@@ -7,80 +7,37 @@
 
 // ########## IMPORTS
 
-// Module to get locator data.
-const {getLocatorData} = require('../procs/getLocatorData');
+// Module to perform common operations.
+const {init, report} = require('../procs/testaro');
 
 // ########## FUNCTIONS
 
+// Runs the test and returns the result.
 exports.reporter = async (page, withItems) => {
-  // Initialize the result.
-  const data = {};
-  const totals = [0, 0, 0, 0];
-  const standardInstances = [];
-  // Get locators for body elements with br descendants.
-  const locAll = page.locator('body *', {has: page.locator('br')});
-  const locs = await locAll.all();
-  // For each of them:
-  for (const loc of locs) {
-    // Get whether it contains adjacent br children without intervening nonspacing text.
-    const hasBrBr = await loc.evaluate(element => {
-      const has2Br = element.querySelector('br + br');
-      if (has2Br) {
-        const childNodes = Array.from(element.childNodes);
-        const nsChildNodes = childNodes.filter(
-          node => node.nodeType !== Node.TEXT_NODE || node.nodeValue.replace(/\s/g, '').length
-        );
-        return nsChildNodes.some(
-          (node, index) => node.nodeName === 'BR' && nsChildNodes[index + 1].nodeName === 'BR'
-        );
-      }
-      else {
-        return false;
-      }
+  // Initialize the locators and result.
+  const all = await init(page, 'body *', {has: page.locator('br + br')});
+  // For each locator:
+  for (const loc of all.allLocs) {
+    // Get whether its element violates the rule.
+    const isBad = await loc.evaluate(el => {
+      const childNodes = Array.from(el.childNodes);
+      const realChildNodes = childNodes.filter(
+        node => node.nodeType !== Node.TEXT_NODE || node.nodeValue.replace(/\s/g, '').length
+      );
+      return realChildNodes.some(
+        (node, index) => node.nodeName === 'BR' && realChildNodes[index + 1].nodeName === 'BR'
+      );
     });
-    // If so:
-    if (hasBrBr) {
-      // Add to the totals.
-      totals[0]++;
-      // If itemization is required:
-      if (withItems) {
-        // Get data on the element.
-        const elData = await getLocatorData(loc);
-        // Add an instance to the result.
-        standardInstances.push({
-          ruleID: 'pseudoP',
-          what: 'Element contains 2 or more adjacent br elements that may be nonsemantic substitute for a p element',
-          ordinalSeverity: 0,
-          tagName: elData.tagName,
-          id: elData.id,
-          location: elData.location,
-          excerpt: elData.excerpt
-        });
-      }
+    // If it does:
+    if (isBad) {
+      // Add the locator to the array of violators.
+      all.locs.push(loc);
     }
   }
-  // If itemization is not required:
-  if (! withItems) {
-    // Add a summary instance to the result.
-    standardInstances.push({
-      ruleID: 'pseudoP',
-      what: 'Elements contain 2 or more adjacent br elements that may be nonsemantic substitutes for p elements',
-      ordinalSeverity: 0,
-      count: totals[0],
-      tagName: '',
-      id: '',
-      location: {
-        doc: '',
-        type: '',
-        spec: ''
-      },
-      excerpt: ''
-    });
-  }
-  // Return the result.
-  return {
-    data,
-    totals,
-    standardInstances
-  };
+  // Populate and return the result.
+  const whats = [
+    'Element contains 2 or more adjacent br elements that may be a pseudo-paragraph',
+    'Elements contain 2 or more adjacent br elements that may be pseudo-paragraphs'
+  ];
+  return await report(withItems, all, 'pseudoP', whats, 0);
 };
