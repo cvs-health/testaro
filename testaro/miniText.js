3b3fa25a9f301f42f38cb2a09b23e4cef097acc5
Jonathan Pool <jonathan.pool@cvshealth.com>
2023-08-14 20:11:07 -0400

    Refactored miniText test

--- testaro/miniText.js
+++ testaro/miniText.js
@@ -5,71 +5,34 @@
   This test reports elements with font sizes smaller than 11 pixels.
 */
 
-// ########## IMPORTS
-
-// Module to get locator data.
-const {getLocatorData} = require('../procs/getLocatorData');
+// Module to perform common operations.
+const {init, report} = require('../procs/testaro');
 
 // ########## FUNCTIONS
 
+// Runs the test and returns the result.
 exports.reporter = async (page, withItems) => {
-  // Initialize the result.
-  const data = {};
-  const totals = [0, 0, 0, 0];
-  const standardInstances = [];
-  // Get locators for the body elements with non-spacing text.
-  const locAll = page.locator('body *:not(script, style):visible', {hasText: /[^\s]+/});
-  const locs = await locAll.all();
-  // For each of them:
-  for (const loc of locs) {
-    // If its font size is smaller that 11 pixels:
-    const tinySize = await loc.evaluate(element => {
-      const styleDec = window.getComputedStyle(element);
+  // Initialize the locators and result.
+  const all = await init(page, 'body *:not(script, style):visible', {hasText: /[^\s]+/});
+  // For each locator:
+  for (const loc of all.allLocs) {
+    // Get the font size of its element if less than 11 pixels.
+    const fontSize = await loc.evaluate(el => {
+      const styleDec = window.getComputedStyle(el);
       const fontSizeString = styleDec.fontSize;
       const fontSize = Number.parseFloat(fontSizeString);
       return fontSize < 11 ? fontSize : null;
     });
-    if (tinySize !== null) {
-      // Add to the totals.
-      totals[2]++;
-      // If itemization is required:
-      if (withItems) {
-        // Get data on the element.
-        const elData = await getLocatorData(loc);
-        // Add an instance to the result.
-        standardInstances.push({
-          ruleID: 'miniText',
-          what: `Element has a font size of ${tinySize} pixels, smaller than 11 pixels`,
-          ordinalSeverity: 2,
-          tagName: elData.tagName,
-          id: elData.id,
-          location: elData.location,
-          excerpt: elData.excerpt
-        });
-      }
+    // If it violates the rule:
+    if (fontSize) {
+      // Add the locator to the array of violators.
+      all.locs.push([loc, fontSize]);
     }
   }
-  // If itemization is not required:
-  if (! withItems) {
-    // Add a summary instance to the result.
-    standardInstances.push({
-      ruleID: 'miniText',
-      what: 'Elements have font sizes smaller than 11 pixels',
-      count: totals[2],
-      ordinalSeverity: 2,
-      tagName: '',
-      id: '',
-      location: {
-        doc: '',
-        type: '',
-        spec: ''
-      },
-      excerpt: ''
-    });
-  }
-  return {
-    data,
-    totals,
-    standardInstances
-  };
+  // Populate and return the result.
+  const whats = [
+    'Element has a font size of __param__ pixels, smaller than 11 pixels',
+    'Elements have font sizes smaller than 11 pixels'
+  ];
+  return await report(withItems, all, 'miniText', whats, 2);
 };
--- testaro/nonTable.js
+++ testaro/nonTable.js
@@ -4,6 +4,34 @@
   This test reports tables used for layout.
 */
 
+// Module to perform common operations.
+const {init, report} = require('../procs/testaro');
+
+// ########## FUNCTIONS
+
+// Runs the test and returns the result.
+exports.reporter = async (page, withItems) => {
+  // Initialize the locators and result.
+  const all = await init(page, 'body *');
+  // For each locator:
+  for (const loc of all.allLocs) {
+    // Get whether its element violates the rule.
+    const isBad = await loc.evaluate(el => {
+      const isViolator = el.tagName.length > 300;
+      return isViolator;
+    });
+    // If it does:
+    if (isBad) {
+      // Add the locator to the array of violators.
+      all.locs.push(loc);
+    }
+  }
+  // Populate and return the result.
+  const whats = ['Itemized description', 'Summary description'];
+  return await report(withItems, all, 'ruleID', whats, 0);
+};
+
+
 // ########## IMPORTS
 
 // Module to get locator data.
