69f98512a4f4bf444210c767cccee5fd7065a913
Jonathan Pool <pool@jpdev.pro>
2023-09-07 20:33:10 -0400

    Continued improving performance but not enough to avoid sampling

--- testaro/distortion.js
+++ testaro/distortion.js
@@ -8,34 +8,26 @@
 // ########## IMPORTS
 
 // Module to perform common operations.
-const {report} = require('../procs/testaro');
+const {init, report} = require('../procs/testaro');
 
 // ########## FUNCTIONS
 
 // Runs the test and returns the result.
 exports.reporter = async (page, withItems) => {
-  // Get an array of violator indexes.
-  const areDistorted = await page.evaluate(() => {
-    const allElements = Array.from(document.querySelectorAll('body *'));
-    return allElements.map(el => {
+  // Initialize the locators and result.
+  const all = await init(page, 'body *');
+  // Get those that have distorting transform styles.
+  for (const loc of all.allLocs) {
+    const isDistorted = await loc.evaluate(el => {
       const styleDec = window.getComputedStyle(el);
       const {transform} = styleDec;
       return transform
       && ['matrix', 'perspective', 'rotate', 'scale', 'skew'].some(key => transform.includes(key));
     });
-  });
-  // Get locators for all body descendants.
-  const allLoc = await page.locator('body *');
-  const allLocs = await allLoc.all();
-  // Get the result.
-  const all = {
-    locs: allLocs.filter((loc, index) => areDistorted[index]),
-    result: {
-      data: {},
-      totals: [0, 0, 0, 0],
-      standardInstances: []  
+    if (isDistorted) {
+      all.locs.push(loc);
     }
-  };
+  }
   // Populate the result.
   const whats = ['Element distorts its text', 'Elements distort their texts'];
   return await report(withItems, all, 'distortion', whats, 1);
