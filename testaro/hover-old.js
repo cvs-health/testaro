183965cd3c17f31e977b396904a3b3abec8b6246
Jonathan Pool <jonathan.pool@cvshealth.com>
2023-09-13 11:08:11 -0400

    Started investigating hover performance

--- testaro/hover-old.js
+++ testaro/hover-old.js
@@ -28,37 +28,6 @@
   types.
 */
 
-// ########## IMPORTS
-
-// Module to perform common operations.
-const {init, report} = require('../procs/testaro');
-
-// ########## FUNCTIONS
-
-// Runs the test and returns the result.
-exports.reporter = async (page, withItems) => {
-  // Initialize the locators and result.
-  const all = await init(page, 'body [aria-controls], body [aria-expanded]');
-  // For each locator:
-  for (const loc of all.allLocs) {
-    // Get whether its element violates the rule.
-    const isBad = await loc.evaluate(el => {
-      
-      const isViolator = ! el.href;
-      return isViolator;
-    });
-    // If it does:
-    if (isBad) {
-      // Add the locator to the array of violators.
-      all.locs.push(loc);
-    }
-  }
-  // Populate and return the result.
-  const whats = ['Itemized description', 'Summary description'];
-  return await report(withItems, all, 'ruleID', whats, 0);
-};
-
-
 // IMPORTS
 
 // Module to get locator data.
