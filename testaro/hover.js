c93d74090ed4e997a66885d072d4f7d7d3f6845a
Jonathan Pool <jonathan.pool@cvshealth.com>
2023-09-13 15:00:32 -0400

    Reimplemented hover

--- testaro/hover.js
+++ testaro/hover.js
@@ -1,11 +1,9 @@
 /*
   hover
-  This test reports unexpected impacts of hovering on the visible page. Impacts are measured by
-  pixel changes outside the hovered element and by unhoverability.
+  This test reports unexpected impacts of hovering.
 
-  The elements that are subjected to hovering (called “triggers”) are the Playwright-visible
-  elements that have 'A', 'BUTTON', or (if not with role=menuitem) 'LI' tag names or have
-  'onmouseenter' or 'onmouseover' attributes.
+  The elements that are subjected to hovering (called “triggers”) are the elements that have
+  aria-controls, aria-expanded, onmouseenter, or onmouseover' attributes.
 
   Despite the delay, the test can make the execution time practical by randomly sampling triggers
   instead of hovering over all of them. When sampling is performed, the results may vary from one
@@ -38,147 +36,39 @@ const {init, report} = require('../procs/testaro');
 // Runs the test and returns the result.
 exports.reporter = async (page, withItems) => {
   // Initialize the locators and result.
-  const all = await init(page, 'body [aria-controls], body [aria-expanded]');
+  const all = await init(
+    page, 'body [aria-controls], body [aria-expanded], body [onmouseenter], body [onmouseover]'
+  );
+  const miscAll = await init(page, 'body *');
+  all.allLocs.push(... miscAll.allLocs);
   // For each locator:
   for (const loc of all.allLocs) {
     // Get whether its element violates the rule.
-    const isBad = await loc.evaluate(el => {
-      
-      const isViolator = ! el.href;
-      return isViolator;
+    const additions = await loc.evaluate(el => {
+      const mouseout = new Event('mouseout');
+      el.dispatchEvent(mouseout);
+      const elementCount0 = document.body.querySelectorAll('*').length;
+      const mouseenter = new Event('mouseenter');
+      el.dispatchEvent(mouseenter);
+      const mouseover = new Event('mouseover');
+      el.dispatchEvent(mouseover);
+      const elementCount1 = document.body.querySelectorAll('*').length;
+      const additions = elementCount1 - elementCount0;
+      return additions;
     });
     // If it does:
-    if (isBad) {
-      // Add the locator to the array of violators.
-      all.locs.push(loc);
+    if (additions !== 0) {
+      // Add the locator and the change of element count to the array of violators.
+      const impact = additions > 0
+        ? `added ${additions} elements to the page`
+        : `subtracted ${- additions} from the page`;
+      all.locs.push([loc, impact]);
     }
   }
   // Populate and return the result.
-  const whats = ['Itemized description', 'Summary description'];
+  const whats = [
+    'Hovering over the element __impact__',
+    'Hovering over elements adds elements to or subtracts elements from the page'
+  ];
   return await report(withItems, all, 'ruleID', whats, 0);
-};
-
-
-// IMPORTS
-
-// Module to get locator data.
-const {getLocatorData} = require('../procs/getLocatorData');
-// Module to draw a sample.
-const {getSample} = require('../procs/sample');
-// Module to get pixel changes between two times.
-const {visChange} = require('../procs/visChange');
-
-// FUNCTIONS
-
-// Performs the hover test and reports results.
-exports.reporter = async (page, withItems, sampleSize = 20) => {
-  // Initialize the result.
-  const data = {};
-  const totals = [0, 0, 0, 0];
-  const standardInstances = [];
-  // Identify the triggers.
-  const selectors = ['a', 'button', 'li:not([role=menuitem])', '[onmouseenter]', '[onmouseover]'];
-  const selectorString = selectors.map(selector => `body ${selector}:visible`).join(', ');
-  const locAll = page.locator(selectorString);
-  const locsAll = await locAll.all();
-  // Get the population-to-sample ratio.
-  const psRatio = Math.max(1, locsAll.length / sampleSize);
-  // Get a sample of the triggers.
-  const sampleIndexes = getSample(locsAll, sampleSize);
-  const sample = sampleIndexes.map(index => locsAll[index]);
-  // For each trigger in the sample:
-  for (const loc of sample) {
-    // Hover over it and get the fractional pixel change.
-    const hoverData = await visChange(page, {
-      delayBefore: 0,
-      delayBetween: 500,
-      exclusion: loc
-    });
-    // If the hovering and measurement succeeded:
-    if (hoverData.success) {
-      // If any pixels changed:
-      if (hoverData.changePercent) {
-        // Get the ordinal severity from the fractional pixel change.
-        const ordinalSeverity = Math.floor(Math.min(3, 0.4 * Math.sqrt(hoverData.changePercent)));
-        // Add to the totals.
-        totals[ordinalSeverity] += psRatio;
-        // If itemization is required:
-        if (withItems) {
-          // Get data on the trigger.
-          const elData = await getLocatorData(loc);
-          // Add an instance to the result.
-          standardInstances.push({
-            ruleID: 'hover',
-            what: 'Hovering over the element changes the page',
-            ordinalSeverity,
-            tagName: elData.tagName,
-            id: elData.id,
-            location: elData.location,
-            excerpt: elData.excerpt
-          });
-        }
-      }
-    }
-    // Otherwise, i.e. if hovering and measurement failed:
-    else {
-      // Add to the totals.
-      totals[3] += psRatio;
-      // If itemization is required:
-      if (withItems) {
-        // Get data on the trigger.
-        const elData = await getLocatorData(loc);
-        // Add an instance to the result.
-        standardInstances.push({
-          ruleID: 'hover',
-          what: 'Element is not hoverable',
-          ordinalSeverity: 3,
-          tagName: elData.tagName,
-          id: elData.id,
-          location: elData.location,
-          excerpt: elData.excerpt
-        });
-      }
-    }
-    // Reload the page to preserve locator integrity.
-    try {
-      await page.reload({timeout: 5000});
-    }
-    catch(error) {
-      console.log('ERROR: page reload timed out');
-    }
-  }
-  // If itemization is not required:
-  if (! withItems) {
-    // For each ordinal severity:
-    for (const index in totals) {
-      // If there were any instances with it:
-      if (totals[index]) {
-        // Add a summary instance to the result.
-        standardInstances.push({
-          ruleID: 'hover',
-          what: 'Hovering over elements changes the page or fails',
-          ordinalSeverity: index,
-          count: Math.round(totals[index]),
-          tagName: '',
-          id: '',
-          location: {
-            doc: '',
-            type: '',
-            spec: ''
-          },
-          excerpt: ''
-        });
-      }
-    }
-  }
-  // Round the totals.
-  totals.forEach((total, index) => {
-    totals[index] = Math.round(totals[index]);
-  });
-  // Return the result.
-  return {
-    data,
-    totals,
-    standardInstances
-  };
 };
