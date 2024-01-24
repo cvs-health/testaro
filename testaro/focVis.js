46197e471587ec9de3cc44b64d8e4eb6a79c062d
Jonathan Pool <pool@jpdev.pro>
2024-01-24 15:50:24 -0800

    Added timeout to focVis test to prevent stalls

--- testaro/focVis.js
+++ testaro/focVis.js
@@ -40,25 +40,30 @@ exports.reporter = async (page, withItems) => {
   // For each locator:
   for (const loc of all.allLocs) {
     // Get how its element violates the rule, if it does.
-    const isBad = await loc.evaluate(el => {
-      const isAbove = el.offsetTop + el.offsetHeight <= 0;
-      const isLeft = el.offsetLeft + el.offsetWidth <= 0;
-      return [isAbove, isLeft];
-    });
-    // If it does:
-    if (isBad[0] || isBad[1]) {
-      // Add the locator to the array of violators.
-      let param;
-      if (isBad[0] && isBad[1]) {
-        param = 'above and to the left of';
+    try {
+      const isBad = await loc.evaluate(el => {
+        const isAbove = el.offsetTop + el.offsetHeight <= 0;
+        const isLeft = el.offsetLeft + el.offsetWidth <= 0;
+        return [isAbove, isLeft];
+      }, {timeout: 500});
+      // If it does:
+      if (isBad[0] || isBad[1]) {
+        // Add the locator to the array of violators.
+        let param;
+        if (isBad[0] && isBad[1]) {
+          param = 'above and to the left of';
+        }
+        else if (isBad[0]) {
+          param = 'above';
+        }
+        else {
+          param = 'to the left of';
+        }
+        all.locs.push([loc, param]);
       }
-      else if (isBad[0]) {
-        param = 'above';
-      }
-      else {
-        param = 'to the left of';
-      }
-      all.locs.push([loc, param]);
+    }
+    catch(error) {
+      console.log(`ERROR analyzing locator for focVis (${error.message})`);
     }
   }
   // Populate and return the result.
