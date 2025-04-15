c531f6691d1c9863982fbfd302b6cf12a7f7d49e
Jonathan Pool <jonathan.pool@cvshealth.com>
2025-04-15 13:46:44 -0500

    finish refactoring validation

--- validation/validateTest.js
+++ validation/validateTest.js
@@ -34,59 +34,89 @@ const fs = require('fs').promises;
 // Module to run Testaro jobs.
 const {doJob} = require('../run');
 
+// CONSTANTS
+
+const job = {
+  id: '250101T0000-aaa-00',
+  what: '',
+  strict: true,
+  standard: 'only',
+  observe: false,
+  device: {
+    id: 'default',
+    windowOptions: {
+      reducedMotion: 'no-preference'
+    }
+  },
+  browserID: 'webkit',
+  timeLimit: 0,
+  creationTimeStamp: '250101T0000',
+  executionTimeStamp: '250101T0000',
+  sendReportTo: '',
+  target: {
+    what: 'page for test validation',
+    url: 'file://to_be_replaced.html'
+  },
+  sources: {
+    script: 'test',
+    batch: 'test',
+    mergeID: 'aaa',
+    requester: ''
+  },
+  acts: []
+};
+
 // FUNCTIONS
 
-// Validates a test.
+// Validates a Testaro test.
 exports.validateTest = async testID => {
-  // Create a job to validate the test.
-  
-  const jobFileNames = await fs.readdir(`${__dirname}/tests/jobs`);
-  for (const jobFileName of jobFileNames.filter(fileName => fileName === `${testID}.json`)) {
-    const jobJSON = await fs.readFile(`${__dirname}/tests/jobs/${jobFileName}`, 'utf8');
-    let report = JSON.parse(jobJSON);
-    // Perform it.
-    report = await doJob(report);
-    // Report whether the end time was reported.
-    const {acts, jobData} = report;
-    if (jobData.endTime && /^(?:\d{2}-){2}\d{2}T\d{2}:\d{2}$/.test(jobData.endTime)) {
-      console.log('Success: End time has been correctly populated');
-    }
-    else {
-      console.log('Failure: End time empty or invalid');
-    }
-    // If the test acts were correctly reported:
-    const testActs = acts.filter(act => act.type && act.type === 'test');
-    if (
-      testActs.length === report.acts.filter(act => act.type === 'test').length
-      && testActs.every(
-        testAct => testAct.expectationFailures !== undefined
-      )
-    ) {
+  // Get data for a job for the test.
+  const jobProperties = require(`./tests/jobProperties/${testID}.json`);
+  // Use the data to complete the job.
+  job.what = `validate Testaro test ${jobProperties.rule}`;
+  job.timeLimit = jobProperties.timeLimit;
+  job.acts = jobProperties.acts;
+  // Perform it.
+  report = await doJob(job);
+  // Report whether the end time was reported.
+  const {acts, jobData} = report;
+  if (jobData.endTime && /^(?:\d{2}-){2}\d{2}T\d{2}:\d{2}$/.test(jobData.endTime)) {
+    console.log('Success: End time has been correctly populated');
+  }
+  else {
+    console.log('Failure: End time empty or invalid');
+  }
+  // If the test acts were correctly reported:
+  const testActs = acts.filter(act => act.type && act.type === 'test');
+  if (
+    testActs.length === report.acts.filter(act => act.type === 'test').length
+    && testActs.every(
+      testAct => testAct.expectationFailures !== undefined
+    )
+  ) {
+    // Report this.
+    console.log('Success: Reports have been correctly populated');
+    // If all expectations were satisfied:
+    if (testActs.every(testAct => testAct.expectationFailures === 0)) {
       // Report this.
-      console.log('Success: Reports have been correctly populated');
-      // If all expectations were satisfied:
-      if (testActs.every(testAct => testAct.expectationFailures === 0)) {
-        // Report this.
-        console.log('######## Success: No failures\n');
-      }
-      // Otherwise, i.e. if not all expectations were satisfied:
-      else {
-        // Report this.
-        console.log(
-          '######## Failure: The test has at least one failure (see “"passed": false” below)\n'
-        );
-        // Output the acts that had failures.
-        console.log(
-          JSON.stringify(
-            acts.filter(act => act.type === 'test' && act.expectationFailures), null, 2
-          )
-        );
-      }
+      console.log('######## Success: No failures\n');
     }
+    // Otherwise, i.e. if not all expectations were satisfied:
     else {
-      console.log('Failure: Reports empty or invalid');
-      console.log(JSON.stringify(acts, null, 2));
+      // Report this.
+      console.log(
+        '######## Failure: The test has at least one failure (see “"passed": false” below)\n'
+      );
+      // Output the acts that had failures.
+      console.log(
+        JSON.stringify(
+          acts.filter(act => act.type === 'test' && act.expectationFailures), null, 2
+        )
+      );
     }
   }
-  return Promise.resolve('');
+  else {
+    console.log('Failure: Act results empty or invalid');
+    console.log(JSON.stringify(acts, null, 2));
+  }
 };
