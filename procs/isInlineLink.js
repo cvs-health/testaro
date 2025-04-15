df03eb86dcd0e1dd3f2bcd1fd633d483a73355eb
Jonathan Pool <jonathan.pool@cvshealth.com>
2025-04-14 20:24:57 -0500

    revise isInlineLink proc

--- procs/isInlineLink.js
+++ procs/isInlineLink.js
@@ -1,5 +1,5 @@
 /*
-  © 2023–2024 CVS Health and/or one of its affiliates. All rights reserved.
+  © 2023–2025 CVS Health and/or one of its affiliates. All rights reserved.
 
   MIT License
 
@@ -29,6 +29,9 @@
   with no other nonspacing text content.
 */
 
+// Returns the normalized text content of an element.
+const realTextOf = element => element.textContent.replace(/\s/g, '');
+
 exports.isInlineLink = async loc => await loc.evaluate(element => {
   // If the element is not a link:
   if (element.tagName !== 'A' && element.getAttribute('role') !== 'link') {
@@ -37,35 +40,15 @@ exports.isInlineLink = async loc => await loc.evaluate(element => {
   }
   // Otherwise, i.e. if it is a link:
   else {
-    let listAncestor;
     // Initialize the link as inline.
     let result = true;
-    // If its display style property is block:
-    const styleDec = window.getComputedStyle(element);
-    if (styleDec.display === 'block') {
+    // If its display style property is block or is tantamount to block:
+    if (
+      window.getComputedStyle(element).display === 'block'
+      || (realTextOf(element.closest('p', 'div', 'ul', 'ol'))) === realTextOf(element)
+    ) {
       // Reclassify the link as non-inline.
       result = false;
-    }
-    // Otherwise, if it is in a list item in a list of at least 2 links:
-    else if (listAncestor = element.closest('ul, ol')) {
-      if (listAncestor.children.length > 1 && Array.from(listAncestor.children).every(child => {
-        const isValidListItem = child.tagName === 'LI';
-        const has1Link = child.querySelectorAll('a').length === 1;
-        return isValidListItem && has1Link;
-      })) {
-        // If the list text is entirely link text:
-        const listText = listAncestor.textContent.replace(/\s/g, '');
-        let linkTextRaw = '';
-        listAncestor.querySelectorAll('a').forEach(link => {
-          linkTextRaw += link.textContent;
-        });
-        const linkText = linkTextRaw.replace(/\s/g, '');
-        console.log(linkText);
-        if (listText === linkText && listText.length) {
-          // Reclassify the link as non-inline.
-          result = false;
-        }
-      }
     }
     // Return the result.
     return result;
